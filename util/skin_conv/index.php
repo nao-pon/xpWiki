5df72a56b845c11d9b2462fb598d1153e1bc8e28
nao-pon <nao-pon>
2006-10-13 13:20:20 +0000

    First step of xpWiki :-)

---
+++ util/skin_conv/index.php
@@ -0,0 +1,6 @@
+<?php
+// plugin converter for xpwiki
+$xoopsOption['nocommon'] = TRUE;
+require ( "../../../../mainfile.php" );
+require ( XOOPS_TRUST_PATH . "/modules/xpwiki/util/skin_conv/index.php" );
+?>
