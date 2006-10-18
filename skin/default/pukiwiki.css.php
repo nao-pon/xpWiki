0076e005b2bc6d962a37e4533169570c3f1c87d1
nao-pon <hypweb@gmail.com>
2006-10-18 05:05:59 +0000

    use mytrustdirname.php

--- skin/default/pukiwiki.css.php
+++ skin/default/pukiwiki.css.php
@@ -2,5 +2,7 @@
 $xoopsOption['nocommon'] = true;
 require '../../../../mainfile.php' ;
 if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH in mainfile.php' ) ;
-include XOOPS_TRUST_PATH."/modules/xpwiki/skin/".basename(__FILE__); 
+
+require '../../mytrustdirname.php' ; // set $mytrustdirname
+include XOOPS_TRUST_PATH.'/modules/'.$mytrustdirname.'/skin/'.basename(__FILE__); 
 ?>
