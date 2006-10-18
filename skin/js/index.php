fd8b8410bae8bb942eaef4a7ac68ce2cd55815ff
nao-pon <nao-pon>
2006-10-18 05:05:59 +0000

    use mytrustdirname.php

--- skin/js/index.php
+++ skin/js/index.php
@@ -1,13 +1,14 @@
 <?php
 //
 // Created on 2006/10/16 by nao-pon http://hypweb.net/
-// $Id: index.php,v 1.1 2006/10/18 02:57:31 nao-pon Exp $
+// $Id: index.php,v 1.2 2006/10/18 05:05:59 nao-pon Exp $
 //
 
 $xoopsOption['nocommon'] = TRUE;
 require '../../../../mainfile.php' ;
 if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH into mainfile.php' ) ;
 
-require XOOPS_TRUST_PATH.'/modules/xpwiki/javascript.php' ;
+require '../../mytrustdirname.php' ; // set $mytrustdirname
+require XOOPS_TRUST_PATH.'/modules/'.$mytrustdirname.'/javascript.php' ;
 
 ?>
