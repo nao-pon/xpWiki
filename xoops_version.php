26fcd6df0f54c382b368b7718ce13937a3d0b385
nao-pon <nao-pon>
2006-10-18 14:34:56 +0000

    Version 0.2.1

--- xoops_version.php
+++ xoops_version.php
@@ -10,7 +10,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 
 $modversion['name'] = $mydirname ;
-$modversion['version'] = "0.2" ;
+$modversion['version'] = "0.2.1" ;
 $modversion['description'] = constant($constpref.'_MODULE_DESCRIPTION') ;
 $modversion['credits'] = "hypweb.net";
 $modversion['author'] = "nao-pon" ;
