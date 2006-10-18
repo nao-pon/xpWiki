1729169cdd25c93f06d77ca63f045c5f89de0235
nao-pon <hypweb@gmail.com>
2006-10-18 02:57:31 +0000

    added WikiHelper.

---
+++ skin/js/index.php
@@ -0,0 +1,13 @@
+<?php
+//
+// Created on 2006/10/16 by nao-pon http://hypweb.net/
+// $Id: index.php,v 1.1 2006/10/18 02:57:31 nao-pon Exp $
+//
+
+$xoopsOption['nocommon'] = TRUE;
+require '../../../../mainfile.php' ;
+if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH into mainfile.php' ) ;
+
+require XOOPS_TRUST_PATH.'/modules/xpwiki/javascript.php' ;
+
+?>
