47a7635ea96a6a8a1f2842bb918873976b30a8ab
nao-pon <hypweb@gmail.com>
2006-10-18 08:05:18 +0000

    moved util(plugin, skin converter) to admin space.

--- util/plugin_conv/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// plugin converter for xpwiki
-$xoopsOption['nocommon'] = TRUE;
-require ( "../../../../mainfile.php" );
-require ( XOOPS_TRUST_PATH . "/modules/xpwiki/util/plugin_conv/index.php" );
-?>
