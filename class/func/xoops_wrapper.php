2cb37a5fc8c7c4a7e7dc1607675071b82ddd3679
nao-pon <hypweb@gmail.com>
2006-10-24 00:16:45 +0000

    regularized "pkwk_mail_notify()" for XOOPS.

--- class/func/xoops_wrapper.php
+++ class/func/xoops_wrapper.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Created on 2006/10/11 by nao-pon http://hypweb.net/
-// $Id: xoops_wrapper.php,v 1.7 2006/10/23 08:13:48 nao-pon Exp $
+// $Id: xoops_wrapper.php,v 1.8 2006/10/24 00:16:45 nao-pon Exp $
 //
 class XpWikiXoopsWrapper extends XpWikiBackupFunc {
 	
@@ -90,5 +90,79 @@ class XpWikiXoopsWrapper extends XpWikiBackupFunc {
 				return $default;
 		}
 	}
+
+	function pkwk_mail_notify($subject, $message, $footer = array())
+	{
+		static $_to, $_headers, $_after_pop;
+	
+		// Init and lock
+		if (! isset($_to[$this->xpwiki->pid])) {
+			if (! $this->cont['PKWK_OPTIMISE']) {
+				// Validation check
+				$func = 'pkwk_mail_notify(): ';
+				$mail_regex   = '/[^@]+@[^@]{1,}\.[^@]{2,}/';
+				//if (! preg_match($mail_regex, $this->root->notify_to))
+				//	die($func . 'Invalid $this->root->notify_to');
+				//if (! preg_match($mail_regex, $this->root->notify_from))
+				//	die($func . 'Invalid $this->root->notify_from');
+				if ($this->root->notify_header != '') {
+					$header_regex = "/\A(?:\r\n|\r|\n)|\r\n\r\n/";
+					if (preg_match($header_regex, $this->root->notify_header))
+						die($func . 'Invalid $this->root->notify_header');
+					if (preg_match('/^From:/im', $this->root->notify_header))
+						die($func . 'Redundant \'From:\' in $this->root->notify_header');
+				}
+			}
+			
+			$_to[$this->xpwiki->pid]      = $this->root->notify_to;
+			$_headers[$this->xpwiki->pid] =
+				'X-Mailer: xpWiki/' . $this->cont['S_VERSION'] .
+				' PHP/' . phpversion() . "\r\n";
+							
+			// Additional header(s) by admin
+			if ($this->root->notify_header != '') $_headers[$this->xpwiki->pid] .= "\r\n" . $this->root->notify_header;
+
+	
+			//$_after_pop[$this->xpwiki->pid] = $this->root->smtp_auth;
+		}
+	
+		if ($subject == '' || ($message == '' && empty($footer))) return FALSE;
+	
+		// Subject:
+		if (isset($footer['PAGE'])) $subject = str_replace('$page', $footer['PAGE'], $subject);
+	
+		// Footer
+		if (isset($footer['REMOTE_ADDR'])) $footer['REMOTE_ADDR'] = & $_SERVER['REMOTE_ADDR'];
+		if (isset($footer['USER_AGENT']))
+			$footer['USER_AGENT']  = '(' . $this->cont['UA_PROFILE'] . ') ' . $this->cont['UA_NAME'] . '/' . $this->cont['UA_VERS'];
+		if (! empty($footer)) {
+			$_footer = '';
+			if ($message != '') $_footer = "\n" . str_repeat('-', 30) . "\n";
+			foreach($footer as $key => $value)
+				$_footer .= $key . ': ' . $value . "\n";
+			$message .= $_footer;
+		}
+	
+		// Wait POP/APOP auth completion
+		//if ($_after_pop[$this->xpwiki->pid]) {
+		//	$result = $this->pop_before_smtp();
+		//	if ($result !== TRUE) die($result);
+		//}
+	
+		global $xoopsConfig;
+		$xoopsMailer =& getMailer();
+		$xoopsMailer->useMail();
+		$xoopsMailer->setFromEmail($xoopsConfig['adminmail']);
+		$xoopsMailer->setFromName($xoopsConfig['sitename']);
+		$xoopsMailer->setSubject($subject);
+		$xoopsMailer->setBody($message);
+		$xoopsMailer->setToEmails($xoopsConfig['adminmail']);
+		$xoopsMailer->headers = explode("\r\n",rtrim($_headers[$this->xpwiki->pid]));
+		$xoopsMailer->send();
+		$xoopsMailer->reset();
+		
+		return true;
+	}
+
 }
 ?>
