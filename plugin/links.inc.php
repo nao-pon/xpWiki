96fe6c181f5710aeffce05aa4ba58604a14934b6
nao-pon <nao-pon>
2006-11-07 00:50:16 +0000

    edited for xpWiki style.

--- plugin/links.inc.php
+++ plugin/links.inc.php
@@ -1,36 +1,44 @@
 <?php
-class xpwiki_plugin_links extends xpwiki_plugin {
-	// PukiWiki - Yet another WikiWikiWeb clone
-	// $Id: links.inc.php,v 1.1 2006/10/13 13:17:49 nao-pon Exp $
-	//
-	// Update link cache plugin
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: links.inc.php,v 1.2 2006/11/07 00:50:16 nao-pon Exp $
+//
+// Update link cache plugin
 	
-	// Message setting
+class xpwiki_plugin_links extends xpwiki_plugin {
+
 	function plugin_links_init()
 	{
 
-
+		// 管理画面モード指定
+		if ($this->root->module['platform'] == "xoops") {
+			$this->root->runmode = "xoops_admin";
+		}
+		
+		// 言語ファイルの読み込み
+		$this->load_language();
+		
+		// Message setting
 		$messages = array(
 			'_links_messages'=>array(
-				'title_update'  => 'キャッシュ更新',
+			'title_update'  => 'キャッシュ更新',
 			'msg_adminpass' => '管理者パスワード',
 			'btn_submit'    => '実行',
 			'msg_done'      => 'キャッシュの更新が完了しました。',
 			'msg_usage'     => "
 * 処理内容
-	
-	:キャッシュを更新|
-	全てのページをスキャンし、あるページがどのページからリンクされているかを調査して、キャッシュに記録します。
-	
-	* 注意
-	実行には数分かかる場合もあります。実行ボタンを押したあと、しばらくお待ちください。
-	
-	* 実行
-	管理者パスワードを入力して、[実行]ボタンをクリックしてください。
-	"
-		)
+
+:キャッシュを更新|
+全てのページをスキャンし、あるページがどのページからリンクされているかを調査して、キャッシュに記録します。
+
+* 注意
+実行には数分かかる場合もあります。実行ボタンを押したあと、しばらくお待ちください。
+
+* 実行
+[実行]ボタンをクリックしてください。
+"
+			)
 		);
-		$this->func->set_plugin_messages($messages);
+		//$this->func->set_plugin_messages($messages);
 	}
 	
 	function plugin_links_action()
@@ -41,17 +49,15 @@ class xpwiki_plugin_links extends xpwiki_plugin {
 		if ($this->cont['PKWK_READONLY']) $this->func->die_message('PKWK_READONLY prohibits this');
 	
 		$msg = $body = '';
-		if (empty($this->root->vars['action']) || empty($this->root->post['adminpass']) || ! $this->func->pkwk_login($this->root->post['adminpass'])) {
-			$msg   = & $this->root->_links_messages['title_update'];
-			$body  = $this->func->convert_html($this->root->_links_messages['msg_usage']);
+		if (empty($this->root->vars['action']) || ! $this->func->pkwk_login()) {
+			$msg   = & $this->msg['title_update'];
+			$body  = $this->func->convert_html($this->msg['msg_usage']);
 			$body .= <<<EOD
 <form method="POST" action="{$this->root->script}">
  <div>
   <input type="hidden" name="plugin" value="links" />
   <input type="hidden" name="action" value="update" />
-  <label for="_p_links_adminpass">{$this->root->_links_messages['msg_adminpass']}</label>
-  <input type="password" name="adminpass" id="_p_links_adminpass" size="20" value="" />
-  <input type="submit" value="{$this->root->_links_messages['btn_submit']}" />
+  <input type="submit" value="{$this->msg['btn_submit']}" />
  </div>
 </form>
 EOD;
@@ -59,11 +65,11 @@ EOD;
 		} else if ($this->root->vars['action'] == 'update') {
 			$this->func->links_init();
 			$this->root->foot_explain = array(); // Exhaust footnotes
-			$msg  = & $this->root->_links_messages['title_update'];
-			$body = & $this->root->_links_messages['msg_done'    ];
+			$msg  = & $this->msg['title_update'];
+			$body = & $this->msg['msg_done'    ];
 		} else {
-			$msg  = & $this->root->_links_messages['title_update'];
-			$body = & $this->root->_links_messages['err_invalid' ];
+			$msg  = & $this->msg['title_update'];
+			$body = & $this->msg['err_invalid' ];
 		}
 		return array('msg'=>$msg, 'body'=>$body);
 	}
