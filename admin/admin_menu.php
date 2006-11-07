a4215a12d269369fecf5902d5f88e7106e9a4ac6
nao-pon <nao-pon>
2006-11-07 04:50:42 +0000

    rename pagename 'AdminTooles' to ':AdminTools'.

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -3,7 +3,7 @@
 $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $adminmenu[0]['title'] = constant( $constpref.'_ADMIN_TOOLS' ) ;
-$adminmenu[0]['link']  = "?AdminTools" ;
+$adminmenu[0]['link']  = "?:AdminTools" ;
 
 $adminmenu[1]['title'] = constant( $constpref.'_PLUGIN_CONVERTER' ) ;
 $adminmenu[1]['link']  = "admin/index.php?page=plugin_conv" ;
