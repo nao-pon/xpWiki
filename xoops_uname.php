bcb20672f6895b414e27c2ebc3bec0c9e002498b
nao-pon <nao-pon>
2006-11-13 11:53:45 +0000

    user name suggest for pginfo plugin.

---
+++ xoops_uname.php
@@ -0,0 +1,18 @@
+<?php
+//
+// Created on 2006/11/13 by nao-pon http://hypweb.net/
+// $Id: xoops_uname.php,v 1.1 2006/11/13 11:53:45 nao-pon Exp $
+//
+
+define('_LEGACY_PREVENT_LOAD_CORE_', TRUE); // for XOOPS Cube Legacy 
+$xoopsOption['nocommon'] = TRUE; // for XOOPS 2
+require '../../mainfile.php';
+
+if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH into mainfile.php' ) ;
+
+$mydirname = basename( dirname( __FILE__ ) ) ;
+$mydirpath = dirname( __FILE__ ) ;
+require $mydirpath.'/mytrustdirname.php' ; // set $mytrustdirname
+
+require XOOPS_TRUST_PATH.'/modules/'.$mytrustdirname.'/xoops_uname.php' ;
+?>
