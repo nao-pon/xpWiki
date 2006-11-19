f68c886022b2b24bc77cc40dc458ebc9d04eafd5
nao-pon <hypweb@gmail.com>
2006-11-19 11:44:15 +0000

    links plugin became needless.

--- lang/plugin/links.en.php
+++ lang/plugin/links.en.php
@@ -1,24 +1,14 @@
 <?php
 //
 // Created on 2006/11/07 by nao-pon http://hypweb.net/
-// $Id: links.en.php,v 1.1 2006/11/07 00:50:16 nao-pon Exp $
+// $Id: links.en.php,v 1.2 2006/11/19 11:44:15 nao-pon Exp $
 //
 $msg = array(
 	'title_update'   => 'Cache update',
-	'msg_adminpass'  => 'Admin pass',
-	'btn_submit'     => 'Go!',
-	'msg_done'       => 'Cache update was completed.',
-	'msg_usage'      => "
-* Processing contents
-
-:Cache update|
-Scan all pages and investigate which page a certain page is linked by and record it to cache.
-
-* Attention
-When it takes several minutes for practice, there is it. Please wait for a while after having pushed a practice button.
-
-* Go
-Please click a [Go!].
-"
+	'body'    =>
+'
+-The link information of pages use a pginfo database.
+-When it updates link information, it is [[cmd:dbsync]] Please be similar.
+'
 );
 ?>
