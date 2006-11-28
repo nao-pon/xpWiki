9b6456acb14ac33b3c536c0fe25aa19518a909b8
nao-pon <nao-pon>
2006-11-28 00:14:56 +0000

    get description from converted page.

--- class/extension/whatsnew.php
+++ class/extension/whatsnew.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Created on 2006/10/29 by nao-pon http://hypweb.net/
-// $Id: whatsnew.php,v 1.3 2006/11/24 13:47:07 nao-pon Exp $
+// $Id: whatsnew.php,v 1.4 2006/11/28 00:14:56 nao-pon Exp $
 //
 
 class XpWikiExtension_whatsnew extends XpWikiExtension {
@@ -20,7 +20,7 @@ class XpWikiExtension_whatsnew extends XpWikiExtension {
 		$recent_dat  = $this->cont['PKWK_MAXSHOW_CACHE'];
 		//$recent_line = @file($this->cont['CACHE_DIR'] . $recent_dat);
 		//$recent_arr  = array_slice($recent_line, 0, $limit);
-		$recent_arr = $this->func->get_existpages_db(FALSE,'',$limit," ORDER BY editedtime DESC",TRUE,FALSE,TRUE,TRUE);
+		$recent_arr = $this->func->get_existpages(FALSE,'',$limit," ORDER BY editedtime DESC",TRUE,FALSE,TRUE,TRUE);
 		
 		foreach($recent_arr as $line) {
 			list($time, $base) = explode("\t", trim($line));
@@ -31,13 +31,14 @@ class XpWikiExtension_whatsnew extends XpWikiExtension {
 			$ret[$i]['time']  = $localtime;
 			
 			// 指定ページの本文取得
-			//$page = new XpWiki($this->root->mydirname);
-			//$page->init($base);
-			//$page->root->rtf['use_cache_always'] = TRUE;
-			//$page->execute();
-			//$desc = $page->body;
+			$page = new XpWiki($this->root->mydirname);
+			$page->init($base);
+			$page->root->rtf['use_cache_always'] = TRUE;
+			$page->execute();
+			$ret[$i]['description'] = strip_tags($page->body);
 			
-			$ret[$i]['description'] = $this->func->get_plain_text_db($base);
+			//$ret[$i]['description'] = $this->func->get_plain_text_db($base);
+			
 			$i++;
 		}
 		return $ret;
