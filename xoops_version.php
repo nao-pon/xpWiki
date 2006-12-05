310ff3939318721a473e8bb83720ec4609a515fa
nao-pon <nao-pon>
2006-12-05 23:56:42 +0000

    Version 0.7.15

--- xoops_version.php
+++ xoops_version.php
@@ -12,7 +12,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 
 $modversion['name'] = $mydirname ;
-$modversion['version'] = "0.7.14" ;
+$modversion['version'] = "0.7.15" ;
 $modversion['description'] = constant($constpref.'_MODULE_DESCRIPTION') ;
 $modversion['credits'] = "&copy; 2006 hypweb.net.";
 $modversion['author'] = "nao-pon" ;
