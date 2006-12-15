8f72b6dde98e865d660142f08250c64497d2989f
nao-pon <nao-pon>
2006-12-15 01:11:51 +0000

    Ver 0.8.4

--- xoops_version.php
+++ xoops_version.php
@@ -12,7 +12,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 
 $modversion['name'] = $mydirname ;
-$modversion['version'] = "0.8.3" ;
+$modversion['version'] = "0.8.4" ;
 $modversion['description'] = constant($constpref.'_MODULE_DESCRIPTION') ;
 $modversion['credits'] = "&copy; 2006 hypweb.net.";
 $modversion['author'] = "nao-pon" ;
