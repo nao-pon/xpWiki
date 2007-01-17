e318da6500167865e65a34ad0e8ab7d9f2814efb
nao-pon <nao-pon>
2007-01-17 23:26:05 +0000

    bugfixed. could not see next result link in xoops global search.

--- search.php
+++ search.php
@@ -5,9 +5,10 @@ eval( '
 function '. $mydirname .'_global_search( $keywords , $andor , $limit , $offset , $userid )
 {
 	// for XOOPS Search module
-	static $readed = FALSE;
-	if($readed) { return array() ; }
-	$readed = TRUE;
+	static $readed = array();
+	$md5 = md5($keywords . $andor . $limit . $offset . $userid);
+	if(isset($readed[$md5])) { return array() ; }
+	$readed[$md5] = TRUE;
 	return xpwiki_global_search_base( "'.$mydirname.'" , $keywords , $andor , $limit , $offset , $userid ) ;
 }
 
@@ -18,21 +19,26 @@ if( ! function_exists( 'xpwiki_global_search_base' ) ) {
 
 function xpwiki_global_search_base( $mydirname , $keywords , $andor , $limit , $offset , $userid )
 {
-	// 必要なファイルの読み込み
-	$mytrustdirpath = dirname( __FILE__ ) ;
-	include_once "$mytrustdirpath/include.php";
 	
-	// XpWiki オブジェクト作成
-	$xpwiki = new XpWiki($mydirname);
+	static $xpwiki = array();
 	
-	// xoopsSearch extension 読み込み
-	$xpwiki->load_extensions("xoopsSearch");
-	
-	// 初期化
-	$xpwiki->init();
+	if (empty($xpwiki[$mydirname])) {
+		// 必要なファイルの読み込み
+		$mytrustdirpath = dirname( __FILE__ ) ;
+		include_once "$mytrustdirpath/include.php";
+		
+		// XpWiki オブジェクト作成
+		$xpwiki[$mydirname] = new XpWiki($mydirname);
+		
+		// xoopsSearch extension 読み込み
+		$xpwiki[$mydirname]->load_extensions("xoopsSearch");
+		
+		// 初期化
+		$xpwiki[$mydirname]->init();
+	}
 	
 	// データ取得
-	return $xpwiki->extension->xoopsSearch->get ( $keywords , $andor , $limit , $offset , $userid );
+	return $xpwiki[$mydirname]->extension->xoopsSearch->get ( $keywords , $andor , $limit , $offset , $userid );
 }
 
 }
