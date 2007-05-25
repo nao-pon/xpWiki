9b17821c964ab6bbf483867bf4a79724435c171b
nao-pon <hypweb@gmail.com>
2007-05-25 02:48:30 +0000

    Moved from onPageWriteBefore to onPageWriteAfter.

--- events/onPageWriteBefore/api.inc.php
+++ events/onPageWriteBefore/api.inc.php
@@ -1,27 +1,8 @@
 <?php
 /*
  * Created on 2007/04/12 by nao-pon http://hypweb.net/
- * $Id: api.inc.php,v 1.1 2007/04/12 05:12:24 nao-pon Exp $
+ * $Id: api.inc.php,v 1.2 2007/05/25 02:48:30 nao-pon Exp $
  */
 
-function xpwiki_onPageWriteBefore_api (&$xpwiki_func, &$page, &$postdata, &$notimestamp, &$mode) {
-
-	if ( $mode !== 'update' ) {
-		// Clear cache *.autolink.api
-		$base = $xpwiki_func->cont['CACHE_DIR'];
-		if (function_exists('glob')) {
-			foreach (glob($base . "*.autolink.api") as $file) {
-				unlink($file);
-			}
-		} else {
-			if ($dir = @opendir($base))
-			{
-				while($file = readdir($dir))
-				{
-					if (substr($file, -13) === '.autolink.api') unlink($base . '/' . $file);
-				}
-			}
-		}
-	}
-}
+// Moved to onPageWriteAfter at 2007/5/22.
 ?>
