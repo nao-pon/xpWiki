a6cbec2895f79d6ce8ae87e3f40494f66b643ae7
nao-pon <hypweb@gmail.com>
2007-05-25 03:07:57 +0000

    Version 2.05

--- xoops_version.php
+++ xoops_version.php
@@ -12,7 +12,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 
 $modversion['name'] = $mydirname ;
-$modversion['version'] = "2.04" ;
+$modversion['version'] = "2.05" ;
 $modversion['description'] = constant($constpref.'_MODULE_DESCRIPTION') ;
 $modversion['credits'] = "&copy; 2006-2007 hypweb.net.";
 $modversion['author'] = "nao-pon" ;
