6cd6f737cdbee6b811d4e1ffc1a479fc0125004b
nao-pon <nao-pon>
2007-05-28 07:59:30 +0000

    Added language 'ja_utf8'.

---
+++ language/ja_utf8/blocks_common.php
@@ -0,0 +1,7 @@
+<?php
+
+// definitions for editing blocks
+define("_MB_XPWIKI_PAGENAME","表示するページ名");
+define("_MB_XPWIKI_WIDTH","ページ内容の表示幅"); 
+define("_MB_XPWIKI_THISTEMPLATE","このブロックのテンプレート"); 
+?>
