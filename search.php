de77efe5a7b7f6c4989d2107798efb220a6ea9bf
nao-pon <hypweb@gmail.com>
2007-06-01 01:05:34 +0000

    Garbage collection

--- search.php
+++ search.php
@@ -19,29 +19,28 @@ if( ! function_exists( 'xpwiki_global_search_base' ) ) {
 
 function xpwiki_global_search_base( $mydirname , $keywords , $andor , $limit , $offset , $userid )
 {
+	// 必要なファイルの読み込み
+	$mytrustdirpath = dirname( __FILE__ ) ;
+	include_once "$mytrustdirpath/include.php";
 	
-	static $xpwiki = array();
+	// XpWiki オブジェクト作成
+	$xpwiki = new XpWiki($mydirname);
 	
-	if (empty($xpwiki[$mydirname])) {
-		// 必要なファイルの読み込み
-		$mytrustdirpath = dirname( __FILE__ ) ;
-		include_once "$mytrustdirpath/include.php";
-		
-		// XpWiki オブジェクト作成
-		$xpwiki[$mydirname] = new XpWiki($mydirname);
-		
-		// xoopsSearch extension 読み込み
-		$xpwiki[$mydirname]->load_extensions("xoopsSearch");
-		
-		// 初期化
-		$xpwiki[$mydirname]->init();
-	}
+	// xoopsSearch extension 読み込み
+	$xpwiki->load_extensions("xoopsSearch");
+	
+	// 初期化
+	$xpwiki->init();
 	
 	// データ取得
-	return $xpwiki[$mydirname]->extension->xoopsSearch->get ( $keywords , $andor , $limit , $offset , $userid );
+	$ret = $xpwiki->extension->xoopsSearch->get ( $keywords , $andor , $limit , $offset , $userid );
+	
+	// オブジェクト破棄
+	$xpwiki = null;
+	
+	return $ret;
 }
 
 }
-
 
 ?>
