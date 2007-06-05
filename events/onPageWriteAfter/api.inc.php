d6b0bb25bf2db8d145bb691bd530f62c092f7f9a
nao-pon <nao-pon>
2007-06-05 00:24:09 +0000

    Chenged for Win32 system.

--- events/onPageWriteAfter/api.inc.php
+++ events/onPageWriteAfter/api.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/05/22 by nao-pon http://hypweb.net/
- * $Id: api.inc.php,v 1.1 2007/05/25 02:48:30 nao-pon Exp $
+ * $Id: api.inc.php,v 1.2 2007/06/05 00:24:09 nao-pon Exp $
  */
 
 function xpwiki_onPageWriteAfter_api (&$xpwiki_func, &$page, &$postdata, &$notimestamp, &$mode) {
@@ -10,15 +10,17 @@ function xpwiki_onPageWriteAfter_api (&$xpwiki_func, &$page, &$postdata, &$notim
 		// Clear cache *.autolink.api
 		$base = $xpwiki_func->cont['CACHE_DIR'];
 		if (function_exists('glob')) {
-			foreach (glob($base . "*.autolink.api") as $file) {
-				unlink($file);
+			chdir($base);
+			foreach (glob("*.autolink.api") as $file) {
+				unlink($base . $file);
 			}
+			chdir($this->cont['DATA_HOME']);
 		} else {
 			if ($dir = @opendir($base))
 			{
 				while($file = readdir($dir))
 				{
-					if (substr($file, -13) === '.autolink.api') unlink($base . '/' . $file);
+					if (substr($file, -13) === '.autolink.api') unlink($base . $file);
 				}
 			}
 		}
