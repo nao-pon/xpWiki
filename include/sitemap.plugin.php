55fb80999f29fb65e9c87361f7b0126e86c64d6c
nao-pon <nao-pon>
2007-07-11 06:14:53 +0000

    For sitemap module of XOOPS.

---
+++ include/sitemap.plugin.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Created on 2007/07/11 by nao-pon http://hypweb.net/
+ * $Id: sitemap.plugin.php,v 1.1 2007/07/11 06:14:53 nao-pon Exp $
+ */
+
+function b_sitemap_xpwiki( $mydirname ) {
+	
+	global $sitemap_configs;
+	
+	$myts =& MyTextSanitizer::getInstance();
+	$ret = array();
+
+	include_once dirname(dirname(__FILE__)).'/include.php';
+
+	$xpwiki =& XpWiki::getSingleton($mydirname);
+	$xpwiki->init('#RenderMode');
+
+	$where = $xpwiki->func->get_readable_where();
+	$where = ($where)? " WHERE (editedtime!=0) AND (name NOT LIKE ':%') AND ($where)" : " WHERE (editedtime!=0) AND (name NOT LIKE ':%')";
+	$query = "SELECT pgid, name, title FROM ".$xpwiki->db->prefix($xpwiki->root->mydirname."_pginfo").$where." ORDER BY editedtime DESC LIMIT 5;";
+	$result = $xpwiki->db->query($query);
+	
+	$ret = array();
+	
+	// Recent Changes
+	$show_cat = (@$sitemap_configs['show_subcategoris'])? 'child' : 'parent';
+	if ($show_cat === 'child') {
+		$ret['id'] = 0;
+		$ret['title'] = $xpwiki->root->_LANG['skin']['recent'];
+		$ret['url'] = '?' . rawurlencode($xpwiki->root->whatsnew);
+	}
+	while( list( $pgid, $page, $title ) = $xpwiki->db->fetchRow( $result ) ) {
+		$title = ($xpwiki->root->pagename_num2str) ? preg_replace('/\/(?:[0-9\-]+|[B0-9][A-Z0-9]{9})$/','/'.$xpwiki->func->get_heading($page),$page) : $page;
+		$ret[$show_cat][] = array(
+			"id" => intval( $pgid ) ,
+			"title" => $myts->makeTboxData4Show($title) ,
+			"url" => $xpwiki->func->get_page_uri($page) ,
+			'image' => 2,
+		) ;
+	}
+	if ($show_cat === 'child') {
+		$ret = array('parent' => array($ret));
+	}
+	
+	// Other menus
+	
+	$ret['parent'][] = array(
+		'id' => 0 ,
+		'title' => $xpwiki->root->_LANG['skin']['list'] ,
+		'url' => '?cmd=list' ,
+	);
+
+	$ret['parent'][] = array(
+		'id' => 0 ,	
+		'title' => $xpwiki->root->_attach_messages['msg_list'],
+		'url' => '?plugin=attach&pcmd=list' ,
+	);
+
+	$ret['parent'][] = array(
+		'id' => 0 ,	
+		'title' => $xpwiki->root->_LANG['skin']['help'],
+		'url' => '?Help' ,
+	);
+	
+	return $ret;
+}
+?>
