65c6bb5fa7ce5680c05652a3987956c6a1f73657
nao-pon <hypweb@gmail.com>
2007-07-12 00:05:46 +0000

    Bugfix. Ploblem time stamp. use buildtime -> editedtime. :-P

--- plugin/recentchanges.inc.php
+++ plugin/recentchanges.inc.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Created on 2006/11/19 by nao-pon http://hypweb.net/
-// $Id: recentchanges.inc.php,v 1.8 2007/07/11 23:53:38 nao-pon Exp $
+// $Id: recentchanges.inc.php,v 1.9 2007/07/12 00:05:46 nao-pon Exp $
 //
 class xpwiki_plugin_recentchanges extends xpwiki_plugin {
 	
@@ -23,7 +23,7 @@ class xpwiki_plugin_recentchanges extends xpwiki_plugin {
 		
 		$where = ($where)? " WHERE (editedtime!=0) AND (name NOT LIKE ':%') AND ($where)" : " WHERE (editedtime!=0) AND (name NOT LIKE ':%')";
 	
-		$query = "SELECT * FROM ".$this->xpwiki->db->prefix($this->root->mydirname."_pginfo").$where." ORDER BY editedtime DESC LIMIT {$this->root->maxshow};";
+		$query = "SELECT `name`, `editedtime` FROM ".$this->xpwiki->db->prefix($this->root->mydirname."_pginfo").$where." ORDER BY editedtime DESC LIMIT {$this->root->maxshow};";
 		$res = $this->xpwiki->db->query($query);
 		
 		if ($res)
@@ -31,16 +31,16 @@ class xpwiki_plugin_recentchanges extends xpwiki_plugin {
 			$date = $items = "";
 			$cnt = 0;
 			$items = '<ol class="list1" style="padding-left:16px;margin-left:16px">';
-			while($data = mysql_fetch_row($res))
+			while(list($page, $editedtime) = mysql_fetch_row($res))
 			{
-				$lastmod = $this->func->format_date($data[3]);
+				$lastmod = $this->func->format_date($editedtime);
 				//$tb_tag = ($this->root->trackback)? "<a href=\"$script?plugin=tb&amp;__mode=view&amp;tb_id=".tb_get_id($data[1])."\" title=\"TrackBack\">TB(".$this->func->tb_count($data[1]).")</a> - " : "";
 				$tb_tag = '';
-				$lasteditor = $this->func->get_lasteditor($this->func->get_pginfo($data[1]));
+				$lasteditor = $this->func->get_lasteditor($this->func->get_pginfo($page));
 				if ($lasteditor) $lasteditor = ' <small>by '.$lasteditor.'</small>';
-				$items .= '<li>'.$this->func->make_pagelink($data[1]).' '.$this->func->get_pg_passage($data[1]).$tb_tag;
+				$items .= '<li>'.$this->func->make_pagelink($page).' '.$this->func->get_pg_passage($page).$tb_tag;
 				$items .= '<ul class="list2"><li>'.$lastmod.$lasteditor;
-				$added = $this->func->get_page_changes($data[1]);
+				$added = $this->func->get_page_changes($page);
 				if ($this->show_recent && $added) {
 					list($added) = explode('&#182;<!--ADD_TEXT_SEP-->',$added);
 					$added = $this->func->drop_submit($added);
