b3f774c46d4961e274cda4b37b8ba3cd15275b06
nao-pon <nao-pon>
2007-09-17 04:36:28 +0000

    Added new config "$const['RENDER_CACHE_DIR']" in pukiwiki.ini.php.

--- events/onPageWriteAfter/render.inc.php
+++ events/onPageWriteAfter/render.inc.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Created on 2007/06/02 by nao-pon http://hypweb.net/
- * $Id: render.inc.php,v 1.2 2007/08/21 06:11:05 nao-pon Exp $
+ * $Id: render.inc.php,v 1.3 2007/09/17 04:36:28 nao-pon Exp $
  */
 
 function xpwiki_onPageWriteAfter_render (&$xpwiki_func, &$page, &$postdata, &$notimestamp, &$mode) {
 
 	if ( $mode !== 'update' ) {
 		// Clear cache render_*
-		$GLOBALS['xpwiki_cache_deletes'][$xpwiki_func->cont['CACHE_DIR']][] = 'render_*';
+		$GLOBALS['xpwiki_cache_deletes'][$xpwiki_func->cont['RENDER_CACHE_DIR']][] = ($xpwiki_func->cont['RENDER_CACHE_DIR'] !== $xpwiki_func->cont['CACHE_DIR'])? '*' : 'render_*';
 	}
 }
 
