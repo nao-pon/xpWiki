8e08dfdf1568bbadf65210759b2694de1e8ad4ca
nao-pon <nao-pon>
2007-11-08 08:30:05 +0000

    Allow Image tag in body option. ( ex. {src:anchor.png,height:12,width:12} uses loader.php)

--- plugin/aname.inc.php
+++ plugin/aname.inc.php
@@ -1,31 +1,31 @@
 <?php
+// PukiWiki - Yet another WikiWikiWeb clone
+// $Id: aname.inc.php,v 1.4 2007/11/08 08:30:05 nao-pon Exp $
+// Copyright (C)
+//   2002-2005 PukiWiki Developers Team
+//   2001-2002 Originally written by yu-ji
+// License: GPL v2 or (at your option) any later version
+//
+// aname plugin - Set various anchor tags
+//   * With just an anchor id: <a id="key"></a>
+//   * With a hyperlink to the anchor id: <a href="#key">string</a>
+//   * With an anchor id and a link to the id itself: <a id="key" href="#key">string</a>
+//
+// NOTE: Use 'id="key"' instead of 'name="key"' at XHTML 1.1
+
 class xpwiki_plugin_aname extends xpwiki_plugin {
 	function plugin_aname_init () {
-
-
-	// PukiWiki - Yet another WikiWikiWeb clone
-	// $Id: aname.inc.php,v 1.3 2007/05/26 01:00:05 nao-pon Exp $
-	// Copyright (C)
-	//   2002-2005 PukiWiki Developers Team
-	//   2001-2002 Originally written by yu-ji
-	// License: GPL v2 or (at your option) any later version
-	//
-	// aname plugin - Set various anchor tags
-	//   * With just an anchor id: <a id="key"></a>
-	//   * With a hyperlink to the anchor id: <a href="#key">string</a>
-	//   * With an anchor id and a link to the id itself: <a id="key" href="#key">string</a>
-	//
-	// NOTE: Use 'id="key"' instead of 'name="key"' at XHTML 1.1
-	
-	// Check ID is unique or not (compatible: no-check)
+		// Check ID is unique or not (compatible: no-check)
 		$this->cont['PLUGIN_ANAME_ID_MUST_UNIQUE'] =  0;
 	
-	// Max length of ID
+		// Max length of ID
 		$this->cont['PLUGIN_ANAME_ID_MAX'] =    40;
 	
-	// Pattern of ID
+		// Pattern of ID
 		$this->cont['PLUGIN_ANAME_ID_REGEX'] =  '/^[A-Za-z][\w\-]*$/';
 
+		// Maxmum image size(px)
+		$this->max_image_size = 32;
 	}
 	
 	// Show usage
@@ -52,7 +52,7 @@ class xpwiki_plugin_aname extends xpwiki_plugin {
 		$convert = TRUE;
 	
 		if (func_num_args() < 1)
-			return $this->func->plugin_aname_usage($convert);
+			return $this->plugin_aname_usage($convert);
 	
 		return $this->plugin_aname_tag(func_get_args(), $convert);
 	}
@@ -63,7 +63,7 @@ class xpwiki_plugin_aname extends xpwiki_plugin {
 		$convert = FALSE;
 	
 		if (func_num_args() < 2)
-			return $this->func->plugin_aname_usage($convert);
+			return $this->plugin_aname_usage($convert);
 	
 		$args = func_get_args(); // ONE or more
 		$body = $this->func->strip_htmltag(array_pop($args), FALSE); // Strip anchor tags only
@@ -80,7 +80,7 @@ class xpwiki_plugin_aname extends xpwiki_plugin {
 		static $_id = array();
 		if (!isset($_id[$this->xpwiki->pid])) {$_id[$this->xpwiki->pid] = array();}
 	
-		if (empty($args) || $args[0] == '') return $this->func->plugin_aname_usage($convert);
+		if (empty($args) || $args[0] == '') return $this->plugin_aname_usage($convert);
 	
 		$id = array_shift($args);
 		$body = '';
@@ -90,23 +90,45 @@ class xpwiki_plugin_aname extends xpwiki_plugin {
 		$f_full  = in_array('full',  $args); // Option: With full(absolute) URI
 	
 		if ($body == '') {
-			if ($f_noid)  return $this->func->plugin_aname_usage($convert, 'Meaningless(No link-title with \'noid\')');
-			if ($f_super) return $this->func->plugin_aname_usage($convert, 'Meaningless(No link-title with \'super\')');
-			if ($f_full)  return $this->func->plugin_aname_usage($convert, 'Meaningless(No link-title with \'full\')');
+			if ($f_noid)  return $this->plugin_aname_usage($convert, 'Meaningless(No link-title with \'noid\')');
+			if ($f_super) return $this->plugin_aname_usage($convert, 'Meaningless(No link-title with \'super\')');
+			if ($f_full)  return $this->plugin_aname_usage($convert, 'Meaningless(No link-title with \'full\')');
 		}
 	
 		if ($this->cont['PLUGIN_ANAME_ID_MUST_UNIQUE'] && isset($_id[$this->xpwiki->pid][$id]) && ! $f_noid) {
-			return $this->func->plugin_aname_usage($convert, 'ID already used: '. $id);
+			return $this->plugin_aname_usage($convert, 'ID already used: '. $id);
 		} else {
 			if (strlen($id) > $this->cont['PLUGIN_ANAME_ID_MAX'])
-				return $this->func->plugin_aname_usage($convert, 'ID too long');
+				return $this->plugin_aname_usage($convert, 'ID too long');
 			if (! preg_match($this->cont['PLUGIN_ANAME_ID_REGEX'], $id))
-				return $this->func->plugin_aname_usage($convert, 'Invalid ID string: ' .
+				return $this->plugin_aname_usage($convert, 'Invalid ID string: ' .
 				htmlspecialchars($id));
 			$_id[$this->xpwiki->pid][$id] = TRUE; // Set
 		}
 	
-		if ($convert) $body = htmlspecialchars($body);
+		if (strpos($body, 'src:') === 0) {
+			$options = array(
+				'src'    => '',
+				'width'  => '',
+				'height' => '',
+				'alt'    => ''
+			);
+			$args = explode(',', $body);
+			$this->fetch_options($options, $args);
+			if (preg_match('/^[a-z0-9_-]+\.(?:png|gif)$/i', $options['src'])) {
+				$height = min($this->max_image_size, intval($options['height']));
+				$height = ($height)? ' height="' . $height . '"' : '';
+				$width = min($this->max_image_size, intval($options['width']));
+				$width = ($width)? ' width="' . $width . '"' : '';
+				$alt = ' alt="' . htmlspecialchars($options['alt']? $options['alt'] : $options['src']) . '"';
+				$body = '<img src="' . $this->cont['LOADER_URL'] . '?src=' . $options['src'] . '"' . $alt . $height . $width . ' />';
+				$convert = FALSE;
+			}
+		}
+		if ($convert) {
+			$body = htmlspecialchars($body);
+		}
+		
 		$id = htmlspecialchars($id); // Insurance
 		$class   = $f_super ? 'anchor_super' : 'anchor';
 		$attr_id = $f_noid  ? '' : ' id="' . $id . '" name="' . $id . '"';
