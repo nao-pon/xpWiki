081fdce89b281aebb6c31599cbc27feced64f689
nao-pon <hypweb@gmail.com>
2007-11-16 09:14:30 +0000

    Added new plugins.

---
+++ lang/plugin/urlbookmark.ja_utf8.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Created on 2007/11/14 by nao-pon http://hypweb.net/
+ * $Id: urlbookmark.ja_utf8.php,v 1.1 2007/11/16 09:14:30 nao-pon Exp $
+ */
+
+$msg = array(
+	'btn_url'                    => 'URL: ',
+	'title_urlbookmark'          => 'タイトル: ',
+	'title_auto'                 => '(自動取得)',
+	'btn_urlbookmark'            => 'URLの追加',
+	'msg_urlbookmark'            => 'コメント(省略可): ',
+	'msg_urlbookmark_collided'   => 'あなたがこのページを編集している間に、他の人が同じページを更新してしまったようです。<br />' .
+			'ブックマークを追加しましたが、違う位置に挿入されているかもしれません。<br />'
+);
+?>
