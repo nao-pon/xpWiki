6e144474a733e450b585d1c4ec4035a753d8ee38
nao-pon <hypweb@gmail.com>
2008-01-03 12:31:49 +0000

    Bugfixed, When describing it two or more times.

--- plugin/renderattach.inc.php
+++ plugin/renderattach.inc.php
@@ -1,17 +1,18 @@
 <?php
 //
-// $Id: renderattach.inc.php,v 1.1 2007/12/30 02:41:34 nao-pon Exp $
+// $Id: renderattach.inc.php,v 1.2 2008/01/03 12:31:49 nao-pon Exp $
 //
 class xpwiki_plugin_renderattach extends xpwiki_plugin {
 	function plugin_renderattach_init () {
+		$this->render_attach = $this->root->render_attach;
 	}
 	
 	function plugin_renderattach_convert()
 	{
 		$args = func_get_args();
 		$page = @ $args[0];
-		if ($this->func->is_page($this->root->render_attach . '/' . $page)) {
-			$this->root->render_attach .= '/' . $page;
+		if ($this->func->is_page($this->render_attach . '/' . $page)) {
+			$this->root->render_attach = $this->render_attach . '/' . $page;
 		}
 		return '';
 	}
