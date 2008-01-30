459e01044459aa9c5f07a809c651abe470494780
nao-pon <nao-pon>
2008-01-30 13:48:49 +0000

    Added a "MenuBar" block.

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -15,6 +15,8 @@ define( $constpref."_BNAME_NOTIFICATION","Notifications ({$mydirname})");
 define( $constpref."_BDESC_NOTIFICATION","Set up about notifications.");
 define( $constpref."_BNAME_FUSEN","Fusen(Tag) ({$mydirname})");
 define( $constpref."_BDESC_FUSEN","The control menu of the Fusen(Tag) plugin is displayed.");
+define( $constpref."_BNAME_MENUBAR","MenuBar ({$mydirname})");
+define( $constpref."_BDESC_MENUBAR","Show MenuBar");
 
 define( $constpref.'_MODULE_DESCRIPTION' , 'A wiki module based on PukiWiki.' ) ;
 
