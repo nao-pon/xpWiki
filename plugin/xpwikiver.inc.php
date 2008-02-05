52d06c56475ba6245d4206ae96e03e69d6cbf751
nao-pon <hypweb@gmail.com>
2008-02-05 08:53:20 +0000

    The function of the plugin was changed from display the version of xpwiki to display the version of the CVS version. The display of the version of xpwiki must use the "version" plugin.

--- plugin/xpwikiver.inc.php
+++ plugin/xpwikiver.inc.php
@@ -1,12 +1,52 @@
 <?php
 //
 // Created on 2006/11/28 by nao-pon http://hypweb.net/
-// $Id: xpwikiver.inc.php,v 1.1 2006/11/28 08:19:25 nao-pon Exp $
+// $Id: xpwikiver.inc.php,v 1.2 2008/02/05 08:53:20 nao-pon Exp $
 //
 class xpwiki_plugin_xpwikiver extends xpwiki_plugin {
-	function plugin_xpwikiver_inline()
-	{
-		return $this->cont['S_VERSION'];
+
+	function plugin_xpwikiver_init() {
+		return $this->time_limit = 1800; // 30 min
+	}
+
+	function plugin_xpwikiver_convert() {
+		return '<p>' . $this->get_ver() . '</p>';
+	}
+
+
+	function plugin_xpwikiver_inline() {
+		return $this->get_ver();
+	}
+	
+	function get_ver() {
+		
+		$c_file = $this->cont['CACHE_DIR'] . 'plugin/xpwikiver.dat';
+		
+		if (file_exists($c_file) && filemtime($c_file) + $this->time_limit > time()) {
+			return join('', file($c_file));
+		}
+		
+		$url = 'http://cvs.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/hypweb/XOOPS_TRUST/modules/xpwiki/xoops_version.php?content-type=text%2Fplain';
+		
+		$dat = $this->func->http_request($url);
+		
+		$ver = '[Error]';
+
+		if ($dat['rc'] === 200) {
+			$data = $dat['data'];
+
+			if (preg_match('/\$modversion\[\'version\'\]\s*=\s*\'([0-9.]+)/', $data, $match)) {
+				$ver = $match[1];
+			}
+	
+		}
+		
+		if ($fp = fopen($c_file,"wb")) {
+			fputs($fp, $ver);
+			fclose($fp);
+		}
+		
+		return $ver;		
 	}
 }
 ?>
