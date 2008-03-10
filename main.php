c29fa270c2465d131d80b96ba8650f832fb04e16
nao-pon <nao-pon>
2008-03-10 04:22:37 +0000

    Clean up.

--- main.php
+++ main.php
@@ -1,33 +1,13 @@
 <?php
 
-// Now debuging....
-// error_reporting(E_ALL);
-
 $mytrustdirname = basename( dirname( __FILE__ ) ) ;
 $mytrustdirpath = dirname( __FILE__ ) ;
-
-// check permission of 'module_read' of this module
-// (already checked by common.php)
-
-// language files
-$language = empty( $xoopsConfig['language'] ) ? 'english' : $xoopsConfig['language'] ;
-if( file_exists( "$mydirpath/language/$language/main.php" ) ) {
-	// user customized language file (already read by common.php)
-	// include_once "$mydirpath/language/$language/main.php" ;
-} else if( file_exists( "$mytrustdirpath/language/$language/main.php" ) ) {
-	// default language file
-	include_once "$mytrustdirpath/language/$language/main.php" ;
-} else {
-	// fallback english
-	include_once "$mytrustdirpath/language/english/main.php" ;
-}
 
 include_once "$mytrustdirpath/include.php";
 
 $xpwiki = new XpWiki($mydirname);
 
 // initialize
-// $xpwiki->init("[Page name]"); (if show a page.)
 $xpwiki->init();
 
 // execute
