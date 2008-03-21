3fc49e45d11fa90a79a93e1f8e77596be98e7706
nao-pon <hypweb@gmail.com>
2008-03-21 05:39:04 +0000

    Version 3.71.5

--- xoops_version.php
+++ xoops_version.php
@@ -9,7 +9,7 @@ if( file_exists( dirname(__FILE__).'/language/'.@$xoopsConfig['language'].'/modi
 $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $modversion['name'] = $mydirname ;
-$modversion['version'] = '3.71.4' ;
+$modversion['version'] = '3.71.5' ;
 $modversion['description'] = constant($constpref.'_MODULE_DESCRIPTION') ;
 $modversion['credits'] = '&copy; 2006-2007 hypweb.net.';
 $modversion['author'] = 'nao-pon' ;
