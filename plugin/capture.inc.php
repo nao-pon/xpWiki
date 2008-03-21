c7561e9707aca49196fa6ed7196f30281671c68a
nao-pon <hypweb@gmail.com>
2008-03-21 04:31:12 +0000

    This is the block reusable capture plugin in the page.

---
+++ plugin/capture.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Created on 2008/03/18 by nao-pon http://hypweb.net/
+ * $Id: capture.inc.php,v 1.1 2008/03/21 04:31:12 nao-pon Exp $
+ */
+
+class xpwiki_plugin_capture extends xpwiki_plugin {
+	function plugin_capture_convert() {
+		
+		$options = array();
+		$args = func_get_args();
+		$this->fetch_options($options, $args, array('name', 'body'));
+		
+		$body = '';
+		if ($options['body']) {
+			$body = str_replace("\r", "\n", $options['body']);
+			$body = $this->func->convert_html($body);
+			$this->root->rtf['capture'][$options['name']] = $body;
+			return '';
+		} else {
+			if (isset($this->root->rtf['capture'][$options['name']])) {
+				$ret = $this->root->rtf['capture'][$options['name']];
+				//unset($this->root->rtf['capture'][$options['name']]);
+				return $ret;
+			}
+		}
+	}
+}
+?>
