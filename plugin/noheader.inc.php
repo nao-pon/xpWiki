21ed5e2be823a8aba9cf1c4314c8645dc57a1da8
nao-pon <nao-pon>
2008-03-25 04:54:27 +0000

    The bug that becomes effective only because it confirms the plug-in is corrected.

--- plugin/noheader.inc.php
+++ plugin/noheader.inc.php
@@ -1,9 +1,7 @@
 <?php
 class xpwiki_plugin_noheader extends xpwiki_plugin {
-	function plugin_noheader_init () {
-		$this->func->add_tag_head('noheader.css');
-	}
 	function plugin_noheader_convert() {
+		$this->func->add_tag_head('noheader.css');
 		return '';
 	}
 }
