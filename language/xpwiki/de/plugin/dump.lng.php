f709acbc28a631dcbdf7d93cc1cd2c9250e31a40
nao-pon <nao-pon>
2008-04-20 01:46:10 +0000

    Added the German language by Octopus. Special thanks for Octopus.

---
+++ language/xpwiki/de/plugin/dump.lng.php
@@ -0,0 +1,32 @@
+<?php
+//
+// Created on 2006/11/06 by nao-pon http://hypweb.net/
+// $Id: dump.lng.php,v 1.1 2008/04/20 01:46:10 nao-pon Exp $
+//
+// German Translation Version 1.0 (11.03.2008)
+// Translation English --> German: Octopus (hunter0815@googlemail.com)
+// sicherlich steckt hier noch reichlich Qualitätspotential in den Übersetzungen ;-)
+
+$msg = array(
+'upload_ok'        => 'Upload abgeschlossen.',
+'upload_ng'        => 'Upload fehlgeschlagen.',
+'maketmp_ng'       => 'Generierung des temporären Files fehlgeschlagen.',
+'file_notfound'    => 'Eine Datei wurde nicht gefunden.',
+'tarfile_notfound' => 'Keine Datei für "untar" gefunden.',
+'filelist'         => 'Liste der "untared" Dateien',
+'data_download'    => 'Download von Daten',
+'file'             => 'Datei:',
+'tar_type'         => 'Archive Typ',
+'tar.gz'           => '*.tar.gz',
+'tar'              => '*.tar',
+'backup_dir'       => 'Backup Ordner',
+'option'           => 'Option',
+'decode_pagename'  => 'Decodiere den Seitennamen mit dem Ordnerpfad.
+ (Dies läßt sich nicht rückgängig machen und einige Buchstaben werden durch \'_\' ersetzt.)',
+'admin_pass'       => 'Passwort des Administrators',
+'password_ng'      => 'das Passwort ist unterschiedlich.',
+'data_restore'     => 'Stelle Daten wieder her (*.tar, *.tar.gz)',
+'data_overwrite'   => '[Warnung] Daten werden überschrieben (please warn a data file of the same name enough).',
+'max_filesize'     => 'Die maximale Dateigröße ist $maxsize KByte.'
+);
+?>
---
+++ language/xpwiki/de/plugin/search.lng.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Created on 2007/05/26 by nao-pon http://hypweb.net/
+ * $Id: search.lng.php,v 1.1 2008/04/20 01:46:10 nao-pon Exp $
+ */
+//
+// German Translation Version 1.0 (11.03.2008)
+// Translation English --> German: Octopus (hunter0815@googlemail.com)
+// sicherlich steckt hier noch reichlich Qualitätspotential in den Übersetzungen ;-)
+
+$msg = array(
+	'btn_search_name' => 'Seiten-Name',
+	'btn_search_text' => 'Suchtext',
+	'btn_search_source' => 'Wiki Quelle',
+);
+?>
