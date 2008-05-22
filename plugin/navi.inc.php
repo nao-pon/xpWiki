2543762d619ceedd792ab4274883cd81aa03b8c1
nao-pon <hypweb@gmail.com>
2008-05-22 08:59:43 +0000

    Added <link rel="next|prev|start"...>.

--- plugin/navi.inc.php
+++ plugin/navi.inc.php
@@ -4,7 +4,7 @@ class xpwiki_plugin_navi extends xpwiki_plugin {
 
 
 	// PukiWiki - Yet another WikiWikiWeb clone.
-	// $Id: navi.inc.php,v 1.8 2008/04/23 23:59:45 nao-pon Exp $
+	// $Id: navi.inc.php,v 1.9 2008/05/22 08:59:43 nao-pon Exp $
 	//
 	// Navi plugin: Show DocBook-like navigation bar and contents
 	
@@ -124,14 +124,17 @@ class xpwiki_plugin_navi extends xpwiki_plugin {
 			if (! $is_home) {
 				$navi[$this->xpwiki->pid][$home]['prev']  = $this->func->make_pagelink($prev);
 				$navi[$this->xpwiki->pid][$home]['prev1'] = $this->func->make_pagelink($prev, $this->root->_navi_prev);
+				$this->func->add_tag_head('<link rel="prev" title="' . htmlspecialchars($prev) . '" href="' . $this->func->get_page_uri($prev, TRUE) . '" />');
 			}
 			if ($next != '') {
 				$navi[$this->xpwiki->pid][$home]['next']  = $this->func->make_pagelink($next);
 				$navi[$this->xpwiki->pid][$home]['next1'] = $this->func->make_pagelink($next, $this->root->_navi_next);
+				$this->func->add_tag_head('<link rel="next" title="' . htmlspecialchars($next) . '" href="' . $this->func->get_page_uri($next, TRUE) . '" />');
 			}
 			$navi[$this->xpwiki->pid][$home]['home']  = $this->func->make_pagelink($home);
 			$navi[$this->xpwiki->pid][$home]['home1'] = $this->func->make_pagelink($home, $this->root->_navi_home);
-	
+			$this->func->add_tag_head('<link rel="start" title="' . htmlspecialchars($home) . '" href="' . $this->func->get_page_uri($home, TRUE) . '" />');
+			
 			// Generate <link> tag: start next prev(previous) parent(up)
 			// Not implemented: contents(toc) search first(begin) last(end)
 			if ($this->cont['PLUGIN_NAVI_LINK_TAGS']) {
