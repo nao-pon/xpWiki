96d78e09e50df6b206a9331ce6afc019ef066c37
nao-pon <nao-pon>
2008-06-09 01:35:40 +0000

    Show title on plugin action.

--- plugin/topicpath.inc.php
+++ plugin/topicpath.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: topicpath.inc.php,v 1.4 2007/09/19 11:27:15 nao-pon Exp $
+// $Id: topicpath.inc.php,v 1.5 2008/06/09 01:35:40 nao-pon Exp $
 //
 // 'topicpath' plugin for PukiWiki, available under GPL
 
@@ -40,35 +40,40 @@ class xpwiki_plugin_topicpath extends xpwiki_plugin {
 		}
 		
 		$page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
-		if ($page === '' || $page === $this->root->defaultpage) return '';
-	
-		$parts = explode('/', $page);
-	
-		$b_link = TRUE;
-		if ($this->cont['PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY']) {
-			$b_link = $this->cont['PLUGIN_TOPICPATH_THIS_PAGE_LINK'];
-		} else {
-			array_pop($parts); // Remove the page itself
-		}
-	
-		$topic_path = array();
-		while (! empty($parts)) {
-			$_landing = join('/', $parts);
-			$landing  = rawurlencode($_landing);
-			$element = htmlspecialchars(array_pop($parts));
-			if (! $b_link)  {
-				// This page ($_landing == $page)
-				$b_link = TRUE;
-				$topic_path[] = $element;
-			} else if ($this->cont['PKWK_READONLY'] && ! $this->func->is_page($_landing)) {
-				// Page not exists
-				$topic_path[] = $element;
+		if ($page === $this->root->defaultpage) return '';
+		
+		if ($page !== '') {
+		
+			$parts = explode('/', $page);
+		
+			$b_link = TRUE;
+			if ($this->cont['PLUGIN_TOPICPATH_THIS_PAGE_DISPLAY']) {
+				$b_link = $this->cont['PLUGIN_TOPICPATH_THIS_PAGE_LINK'];
 			} else {
-				// Page exists or not exists
-				//$topic_path[] = '<a href="' . $this->root->script . '?' . $landing . '">' .
-				//$element . '</a>';
-				$topic_path[] = $this->func->make_pagelink($_landing, $element);
+				array_pop($parts); // Remove the page itself
 			}
+		
+			$topic_path = array();
+			while (! empty($parts)) {
+				$_landing = join('/', $parts);
+				$landing  = rawurlencode($_landing);
+				$element = htmlspecialchars(array_pop($parts));
+				if (! $b_link)  {
+					// This page ($_landing == $page)
+					$b_link = TRUE;
+					$topic_path[] = $element;
+				} else if ($this->cont['PKWK_READONLY'] && ! $this->func->is_page($_landing)) {
+					// Page not exists
+					$topic_path[] = $element;
+				} else {
+					// Page exists or not exists
+					//$topic_path[] = '<a href="' . $this->root->script . '?' . $landing . '">' .
+					//$element . '</a>';
+					$topic_path[] = $this->func->make_pagelink($_landing, $element);
+				}
+			}
+		} else {
+			$topic_path[] = strip_tags($this->xpwiki->title);
 		}
 	
 		if ($this->cont['PLUGIN_TOPICPATH_TOP_DISPLAY'])
