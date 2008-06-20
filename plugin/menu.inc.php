c3a9330f6ef7b2f8e32b6c4fb4549fbd560c4183
nao-pon <hypweb@gmail.com>
2008-06-20 02:09:25 +0000

    Added action method with pagename.

--- plugin/menu.inc.php
+++ plugin/menu.inc.php
@@ -6,7 +6,7 @@ class xpwiki_plugin_menu extends xpwiki_plugin {
 	/////////////////////////////////////////////////
 	// PukiWiki - Yet another WikiWikiWeb clone.
 	//
-	// $Id: menu.inc.php,v 1.7 2008/02/11 00:23:04 nao-pon Exp $
+	// $Id: menu.inc.php,v 1.8 2008/06/20 02:09:25 nao-pon Exp $
 	//
 	
 	// サブメニューを使用する
@@ -15,6 +15,72 @@ class xpwiki_plugin_menu extends xpwiki_plugin {
 	// サブメニューの名称
 		$this->cont['MENU_SUBMENUBAR'] =  'MenuBar';
 
+	}
+	
+	function make_pgmenu ($cmd, $page) {
+		
+		if ($cmd === 'upload') {
+			$docmd = '?plugin=attach&amp;pcmd=' . $cmd;
+		} else if ($cmd === 'attaches') {
+			$docmd = '?plugin=attach&amp;pcmd=list';
+		} else {
+			$docmd = '?cmd=' . $cmd;
+		}
+		return '<a href="' . $this->cont['HOME_URL'] . $docmd . '&amp;page=' . rawurlencode($page) . '">' . $this->root->_LANG['skin'][$cmd] . '</a>';
+	}
+	
+	function plugin_menu_action() {
+		
+		$msg = 'Menu';
+		
+		$page_menus = array();
+		if (isset($this->root->vars['refer']) && $this->root->vars['refer'] !== '') {
+			$_page = $this->root->vars['refer'];
+			$msg = htmlspecialchars($_page) . ' - Menu';
+			
+			$is_editable =  $this->func->check_editable($_page, FALSE, FALSE);
+			$is_freeze = $this->func->is_freeze($_page);
+			$is_admin = $this->root->userinfo['admin'];
+			$is_owner = $this->func->is_owner($_page);
+			$rw    = ! $this->cont['PKWK_READONLY'];
+			$use_attach = ((bool)ini_get('file_uploads') && $rw && $this->func->is_page($_page) && $this->func->get_plugin_instance('attach'));
+			$can_attach = ($use_attach && (! $this->cont['ATTACH_UPLOAD_ADMIN_ONLY'] || $is_admin) && (! $this->cont['ATTACH_UPLOAD_EDITER_ONLY'] || $is_editable));
+			
+			$page_menus[] = '';
+			
+			if (!$is_freeze && $is_editable) {
+				$page_menus[] = $this->make_pgmenu('edit', $_page);
+			}
+			
+			if ($this->root->function_freeze) {
+				$page_menus[] = (! $is_freeze) ? $this->make_pgmenu('freeze', $_page) : $this->make_pgmenu('unfreeze', $_page);
+			}
+			
+			if ($is_owner) {
+				$page_menus[] = $this->make_pgmenu('pginfo', $_page);
+			}
+			
+			$page_menus[] = $this->make_pgmenu('diff', $_page);
+			
+			if ($this->root->do_backup) {
+				$page_menus[] = $this->make_pgmenu('backup', $_page);
+			}
+			
+			if ($can_attach) {
+				$page_menus[] = $this->make_pgmenu('upload', $_page);
+			}
+			
+			if ($use_attach) {
+				$page_menus[] = $this->make_pgmenu('attaches', $_page);
+			}
+			
+			$page_menus[] = '';
+		}
+		$page_menu = ($page_menus) ? '<h4>[ ' . $this->func->make_pagelink($_page) . ' ] Page Menu</h4><div>' . join(' | ', $page_menus)  . '</div><hr />': '';
+		
+		$body = $this->plugin_menu_convert();
+		
+		return array('msg' => $msg, 'body' => $page_menu . $body);
 	}
 	
 	function plugin_menu_convert()
