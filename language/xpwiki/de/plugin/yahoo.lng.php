b8b8f3e36205efa3dee54d400135bce65a281ba0
nao-pon <hypweb@gmail.com>
2008-08-30 06:32:38 +0000

    Yahoo! Web API's plugin.

---
+++ language/xpwiki/de/plugin/yahoo.lng.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Created on 2008/08/30 by nao-pon http://hypweb.net/
+ * License: GPL v2 or (at your option) any later version
+ * $Id: yahoo.lng.php,v 1.1 2008/08/30 06:32:38 nao-pon Exp $
+ */
+
+$msg = array(
+	'msg_notfound' => '# It was not found though %2$s of Yahoo "%1$s" was retrieved.',
+	'msg_more'     => 'Looks for %2$s of "%1$s" more.',
+	'msg_web'      => 'Web site',
+	'msg_img'      => 'Image',
+	'msg_mov'      => 'Movie',
+	'err_badres'   => 'Connected failure to Yahoo!.',
+	'err_option'   => '#yahoo(mode,query) Error: The option is not correctly specified.',
+);
+?>
