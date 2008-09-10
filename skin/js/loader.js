eb45b04fa7a1e97270db46c86916067b8ff5f53c
nao-pon <hypweb@gmail.com>
2008-09-10 04:32:22 +0000

    Correction for low load.

--- skin/js/loader.js
+++ skin/js/loader.js
@@ -1,6 +1,6 @@
 //
 // Created on 2007/10/03 by nao-pon http://hypweb.net/
-// $Id: loader.js,v 1.4 2008/08/23 05:44:30 nao-pon Exp $
+// $Id: loader.js,v 1.5 2008/09/10 04:33:12 nao-pon Exp $
 //
 
 //// JavaScript optimizer by amachang.
@@ -59,30 +59,24 @@ window.clearInterval = function(id) {
 
 // Init.
 var wikihelper_WinIE = (document.all&&!window.opera&&navigator.platform=="Win32");
-var wikihelper_Gecko = navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1;
+var wikihelper_Gecko = (navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1);
 var wikihelper_Opera = !!window.opera;
+var wikihelper_WebKit = (navigator.userAgent.indexOf('AppleWebKit/') > -1);
 
 // prototype.js
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=prototype.js"></script>');
-
 // script.aculo.us
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=effects.js"></script>');
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=dragdrop.js"></script>');
-
 // resizable.js
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=resizable.js"></script>');
-
-// Include main script.
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=main.js"></script>');
+// xpwiki.js
+// main.js
+var xpwiki_scripts = 'prototype,effects,dragdrop,resizable,xpwiki,main';
 
 // Branch.
 if (wikihelper_WinIE) {
-	document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=winie.js"></script>');
-} else if (wikihelper_Gecko) {
-	document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=gecko.js"></script>');
+	xpwiki_scripts += ',winie';
+} else if (wikihelper_Gecko || wikihelper_Opera || wikihelper_WebKit) {
+	xpwiki_scripts += ',basic';
 } else {
-	document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=other.js"></script>');
+	xpwiki_scripts += ',other';
 }
 
-// xpwiki.js
-document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=xpwiki.js"></script>');
+document.write ('<script type="text/javascript" src="' + wikihelper_root_url + '/skin/loader.php?src=' + xpwiki_scripts + '.js"></script>');
