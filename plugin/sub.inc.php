359602ec8984258997e04218c7c90126f63f7c7d
nao-pon <nao-pon>
2009-04-04 07:10:57 +0000

    Allow style &sub(<TEXT); $sup(<TEXT>);

--- plugin/sub.inc.php
+++ plugin/sub.inc.php
@@ -2,21 +2,20 @@
 /*
  * Created on 2008/12/06 by nao-pon http://hypweb.net/
  * License: GPL v2 or (at your option) any later version
- * $Id: sub.inc.php,v 1.1 2008/12/08 23:39:54 nao-pon Exp $
+ * $Id: sub.inc.php,v 1.2 2009/04/04 07:10:57 nao-pon Exp $
  */
 
 class xpwiki_plugin_sub extends xpwiki_plugin {
 	function plugin_sub_inline() {
-		if (func_num_args() !== 1) {
-			return FALSE;
+		$args = func_get_args();
+		$body = array_pop($args);
+		if ($body === ''){
+			if (isset($args[0])) {
+				$body = htmlspecialchars($args[0]);
+			} else {
+				return FALSE;
+			}
 		}
-		
-		list($body) = func_get_args();
-		
-		if ($body == ''){
-			return FALSE;
-		}
-	
 		return '<sub>' . $body . '</sub>';
-	}	
+	}
 }
