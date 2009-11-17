8f2b54157e3dad5932e62a6a276d90bf95f906a8
nao-pon <nao-pon>
2009-11-17 09:15:45 +0000

    Bug-fix about option 'notemplate'.

--- plugin/ls2.inc.php
+++ plugin/ls2.inc.php
@@ -1,7 +1,7 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: ls2.inc.php,v 1.16 2009/03/13 08:18:49 nao-pon Exp $
+// $Id: ls2.inc.php,v 1.17 2009/11/17 09:15:45 nao-pon Exp $
 //
 // List plugin 2
 
@@ -22,13 +22,13 @@
 
 class xpwiki_plugin_ls2 extends xpwiki_plugin {
 	function plugin_ls2_init () {
-	
+
 		// 見出しアンカーの書式
 		$this->cont['PLUGIN_LS2_ANCHOR_PREFIX'] =  '#content_1_';
-	
+
 		// 見出しアンカーの開始番号
 		$this->cont['PLUGIN_LS2_ANCHOR_ORIGIN'] =  0;
-	
+
 		// 見出しレベルを調整する(デフォルト値)
 		$this->cont['PLUGIN_LS2_LIST_COMPACT'] =  FALSE;
 
@@ -49,9 +49,9 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			'_args'       => array()
 		);
 	}
-	
+
 	function plugin_ls2_action() {
-		
+
 		$params = $this->params;
 
 		foreach (array_keys($params) as $key) {
@@ -80,40 +80,40 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			}
 		}
 		$params['_link_query'] = '?' . join('&amp;', $tmp);
-		
+
 		$prefix = isset($this->root->vars['prefix']) ? $this->root->vars['prefix'] : '';
-		
+
 		$params['_base_lev'] = substr_count($prefix, '/');
-		
+
 		$body = $this->plugin_ls2_show_lists($prefix, $params);
-	
+
 		return array('body'=>$body,
 		'msg'=>str_replace('$1', htmlspecialchars($prefix), $this->root->_ls2_msg_title));
 	}
-	
+
 	function can_call_otherdir_convert() {
 		return 1;
 	}
 
 	function plugin_ls2_convert() {
 		$params = $this->params;
-	
+
 		$args = array();
 		if (func_num_args()) {
 			$args   = func_get_args();
 		}
-		
+
 		$this->fetch_options($params, $args, array('prefix'));
-		
+
 		// compat
 		if ($params['basename']) $params['pagename'] = TRUE;
-		
+
 		$prefix = ($params['prefix'])? $prefix = $params['prefix'] : '';
 
 		if ($prefix === '') $prefix = $this->func->strip_bracket($this->cont['PageForRef']) . '/';
 		if ($prefix === '/') $prefix = '';
 		$params['_base_lev'] = substr_count($prefix, '/');
-		
+
 		$title = (! empty($params['_args'])) ? htmlspecialchars(join(',', $params['_args'])) :   // Manual
 			str_replace('$1', htmlspecialchars($prefix), $this->root->_ls2_msg_title); // Auto
 
@@ -134,19 +134,19 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 
 		if (! $params['link'])
 			return $this->plugin_ls2_show_lists($prefix, $params);
-	
+
 		$tmp = array();
 		$tmp[] = 'plugin=ls2&amp;prefix=' . rawurlencode($prefix);
 		if ($params['title'])   $tmp[] = 'title=1';
 		if ($params['include']) $tmp[] = 'include=1';
-	
+
 		return '<p><a href="' . $this->root->script . str_replace('$prefix', rawurlencode($prefix), $params['_link_query']) . '">' .
 		$title . '</a></p>' . "\n";
 	}
-	
+
 	function plugin_ls2_show_lists($prefix, & $params) {
 		static $_auto_template_name;
-		
+
 		$pages = array();
 		$options = array();
 		if ($params['nonlist']) {
@@ -161,7 +161,7 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			//natcasesort($pages);
 			$this->func->pagesort($pages);
 		}
-		
+
 		$base_pages = array();
 		if ($prefix) {
 			$_base = '';
@@ -171,10 +171,10 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 				$_base .= '/';
 			}
 		}
-		
+
 		$params['_parent_depth'] = substr_count(rtrim($prefix,'/'), '/');
 		$params['_child_counts'] = array();
-		
+
 		// テンプレートページの正規表現
 		if (!isset($_auto_template_name[$this->root->mydirname])) {
 			$_temps = array();
@@ -183,12 +183,11 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 					$_temp = array($_temp);
 				}
 				foreach($_temp as $__temp) {
-					if (preg_match('#/([^/\\\\]+)$#', $__temp, $match)) {
-						$_temps[] = preg_quote($match[1]);
-					}
+					$__temp = preg_replace('/\\\\[\d]+/', '', $__temp);
+					$_temps[$__temp] = preg_quote($__temp, '/');
 				}
 			}
-			$_auto_template_name[$this->root->mydirname] = ($_temps)? '/\/(?:'.join('|', $_temps).')(_m)?$/' : '/(?!)/';
+			$_auto_template_name[$this->root->mydirname] = ($_temps)? '/(?:'.join('|', $_temps).')/' : '/(?!)/';
 		}
 
 		foreach ($pages as $key => $page) {
@@ -199,13 +198,13 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 					continue;
 				}
 			}
-			
+
 			// テンプレートページは表示しない場合
-			if ($params['notemplate'] && preg_match("/\/".$_auto_template_name."(_m)?$/",$page)) {
+			if ($params['notemplate'] && preg_match($_auto_template_name[$this->root->mydirname], $page)) {
 				unset($pages[$key]);
 				continue;
 			}
-			
+
 			// 階層深さ指定チェック
 			if ($params['depth'] !== FALSE) {
 				if (substr_count($page, '/') - $params['_parent_depth'] > intval($params['depth'])) {
@@ -241,15 +240,15 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			return $ret.'<div style="clear:left;"></div>';
 		}
 	}
-	
+
 	function plugin_ls2_get_headings($page, & $params, $level, $include = FALSE) {
 		static $_ls2_anchor = array();
 		if (!isset($_ls2_anchor[$this->xpwiki->pid])) {$_ls2_anchor[$this->xpwiki->pid] = 0;}
-	
+
 		// ページが未表示のとき
 		$is_done = (isset($params["page_$page"]) && $params["page_$page"] > 0);
 		if (! $is_done) $params["page_$page"] = ++$_ls2_anchor[$this->xpwiki->pid];
-	
+
 		$r_page = rawurlencode($page);
 		$s_page = htmlspecialchars($page);
 		$title  = $s_page . ' ' . $this->func->get_pg_passage($page, FALSE);
@@ -259,30 +258,30 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			$s_page = $this->func->basename($s_page);
 			$margin_left = (substr_count($page, '/') - $params['_base_lev']) * $this->root->_ul_margin;
 		}
-		
+
 		$href = $this->func->get_page_uri($page, true);
 
 		// New!
 		$new_mark = '';
 		if ($this->func->is_page($page) && !$params['nonew'] && $this->func->exist_plugin_inline("new"))
 			$new_mark = $this->func->do_plugin_inline("new","{$page},nolink");
-		
+
 		// Child count
 		$child_count = (!empty($params['_child_counts'][$page]) && ($params['depth'] !== FALSE) && (substr_count($page, '/') - $params['_parent_depth']) === intval($params['depth']))? ' [<a href="' . $this->root->script .  str_replace('$prefix', rawurlencode($page.'/'), $params['_link_query']) . '">+' . $params['_child_counts'][$page] . '</a>]' : '';
-		
+
 		// Related count
 		$rel_count = ($params['relatedcount'])? ' ('.$this->func->links_get_related_count($page).')' : '';
 
 		$this->plugin_ls2_list_push($params, $level);
 		$ret = $include ? '<li style="margin-left:'.$margin_left.'px;">include ' : '<li style="margin-left:'.$margin_left.'px;">';
-	
+
 		if ($params['title'] && $is_done) {
 			$ret .= '<a href="' . $href . '" title="' . $title . '">' . $s_page . '</a> ';
 			$ret .= '<a href="#list_' . $params["page_$page"] . '"><sup>&uarr;</sup></a>';
 			array_push($params['result'], $ret);
 			return;
 		}
-		
+
 		if ($this->func->is_page($page)) {
 			$ret .= '<a id="list_' . $params["page_$page"] . '" href="' . $href .
 				'" title="' . $title . '">' . $s_page . '</a>' . $rel_count . $child_count . $new_mark;
@@ -290,9 +289,9 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			$_dirname = $this->func->page_dirname($page);
 			$ret .= '<span id="list_' . $params["page_$page"] . '">' . $this->func->make_pagelink($page, (($_dirname && $params['pagename'])? '#compact:'.$_dirname : '')) . '</span>' . $rel_count . $child_count . $new_mark;
 		}
-		
+
 		array_push($params['result'], $ret);
-	
+
 		$anchor = $this->cont['PLUGIN_LS2_ANCHOR_ORIGIN'];
 		$matches = array();
 		foreach ($this->func->get_source($page) as $line) {
@@ -311,7 +310,7 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			}
 		}
 	}
-	
+
 	//リスト構造を構築する
 	function plugin_ls2_list_push(& $params, $level) {
 
@@ -320,19 +319,19 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 		$cont   = TRUE;
 		$open   = '<ul%s>';
 		$close  = '</li></ul>';
-	
+
 		while (count($saved) > $level || (! empty($saved) && $saved[0] != $close))
 			array_push($result, array_shift($saved));
-	
+
 		$margin = $level - count($saved);
-	
+
 		// count($saved)を増やす
 		while (count($saved) < ($level - 1)) array_unshift($saved, '');
-	
+
 		if (count($saved) < $level) {
 			$cont = FALSE;
 			array_unshift($saved, $close);
-	
+
 			$left = ($level == $margin) ? $this->root->_ul_left_margin : 0;
 			if ($params['compact']) {
 				$left  += $this->root->_ul_margin;   // マージンを固定
@@ -343,7 +342,7 @@ class xpwiki_plugin_ls2 extends xpwiki_plugin {
 			$str = sprintf($this->root->_list_pad_str, $level, $left, $left);
 			array_push($result, sprintf($open, $str));
 		}
-	
+
 		if ($cont) array_push($result, '</li>');
 	}
 
