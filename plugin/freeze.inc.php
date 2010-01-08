aa7238e52f048b6095cbbb0767965413efb4cd51
nao-pon <hypweb@gmail.com>
2010-01-08 13:47:11 +0000

    Correction of return function.

--- plugin/freeze.inc.php
+++ plugin/freeze.inc.php
@@ -1,52 +1,45 @@
 <?php
+// PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: freeze.inc.php,v 1.9 2010/01/08 13:47:11 nao-pon Exp $
+//
+// Freeze(Lock) plugin
+
+// Reserve 'Do nothing'. '^#freeze' is for internal use only.
+
 class xpwiki_plugin_freeze extends xpwiki_plugin {
 	function plugin_freeze_init () {
 
-
-
 	}
-	// PukiWiki - Yet another WikiWikiWeb clone.
-	// $Id: freeze.inc.php,v 1.8 2008/11/17 02:34:23 nao-pon Exp $
-	//
-	// Freeze(Lock) plugin
-	
-	// Reserve 'Do nothing'. '^#freeze' is for internal use only.
 	function plugin_freeze_convert() { return ''; }
-	
+
 	function plugin_freeze_action()
 	{
-	//	global $script, $vars, $function_freeze;
-	//	global $_title_isfreezed, $_title_freezed, $_title_freeze;
-	//	global $_msg_invalidpass, $_msg_freezing, $_btn_freeze;
-	
 		$page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
 		if (! $this->root->function_freeze || ! $this->func->is_page($page))
 			return array('msg' => '', 'body' => '');
-	
+
 		$pass = isset($this->root->vars['pass']) ? $this->root->vars['pass'] : NULL;
 		$msg = $body = '';
 		if ($this->func->is_freeze($page)) {
 			// Freezed already
 			$msg  = & $this->root->_title_isfreezed;
 			$body = str_replace('$1', $this->func->make_pagelink($page), $this->root->_title_isfreezed);
-	
+
 		} else if ($this->func->is_owner($page) || ($pass !== NULL && $this->func->pkwk_login($pass))) {
 			// Freeze
 			$postdata = $this->func->get_source($page);
 			array_unshift($postdata, "#freeze\n");
 			$this->root->rtf['no_checkauth_on_write'] = true;
 			$this->func->file_write($this->cont['DATA_DIR'], $page, join('', $postdata), TRUE);
-	
+
 			// pginfo DB write
 			$this->func->pginfo_freeze_db_write($page, 1);
 
 			// Update
 			$this->func->is_freeze($page, TRUE);
-			
-			$this->root->vars['cmd'] = 'read';
-			$msg  = & $this->root->_title_freezed;
-			$body = '';
-	
+
+			$msg = $this->root->_title_freezed;
+
 		} else {
 			// Show a freeze form
 			$msg    = & $this->root->_title_freeze;
@@ -65,7 +58,6 @@ class xpwiki_plugin_freeze extends xpwiki_plugin {
 </form>
 EOD;
 		}
-	
 		return array('msg'=>$msg, 'body'=>$body);
 	}
 }
--- plugin/unfreeze.inc.php
+++ plugin/unfreeze.inc.php
@@ -1,35 +1,31 @@
 <?php
+// PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: unfreeze.inc.php,v 1.9 2010/01/08 13:47:12 nao-pon Exp $
+//
+// Unfreeze(Unlock) plugin
+
+// Show edit form when unfreezed
+
 class xpwiki_plugin_unfreeze extends xpwiki_plugin {
 	function plugin_unfreeze_init () {
 
-
-	// PukiWiki - Yet another WikiWikiWeb clone.
-	// $Id: unfreeze.inc.php,v 1.8 2008/11/17 02:34:23 nao-pon Exp $
-	//
-	// Unfreeze(Unlock) plugin
-	
-	// Show edit form when unfreezed
 		$this->cont['PLUGIN_UNFREEZE_EDIT'] =  TRUE;
 
 	}
-	
+
 	function plugin_unfreeze_action()
 	{
-	//	global $script, $vars, $function_freeze;
-	//	global $_title_isunfreezed, $_title_unfreezed, $_title_unfreeze;
-	//	global $_msg_invalidpass, $_msg_unfreezing, $_btn_unfreeze;
-	
 		$page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
 		if (! $this->root->function_freeze || ! $this->func->is_page($page))
 			return array('msg' => '', 'body' => '');
-	
+
 		$pass = isset($this->root->vars['pass']) ? $this->root->vars['pass'] : NULL;
-		$msg = $body = '';
+		$redirect = $msg = $body = '';
 		if (! $this->func->is_freeze($page)) {
 			// Unfreezed already
 			$msg  = & $this->root->_title_isunfreezed;
 			$body = str_replace('$1', $this->func->make_pagelink($page), $this->root->_title_isunfreezed);
-	
+
 		} else if ($this->func->is_owner($page) || ($pass !== NULL && $this->func->pkwk_login($pass))) {
 			// Unfreeze
 			$postdata = $this->func->get_source($page);
@@ -37,23 +33,20 @@ class xpwiki_plugin_unfreeze extends xpwiki_plugin {
 			$postdata = join('', $postdata);
 			$this->root->rtf['no_checkauth_on_write'] = true;
 			$this->func->file_write($this->cont['DATA_DIR'], $page, $postdata, TRUE);
-	
+
 			// pginfo DB write
 			$this->func->pginfo_freeze_db_write ($page, 0);
 
-			// Update 
+			// Update
 			$this->func->is_freeze($page, TRUE);
 
 			if ($this->cont['PLUGIN_UNFREEZE_EDIT']) {
-				$this->root->vars['cmd'] = 'read'; // To show 'Freeze' link
-				$msg  = & $this->root->_title_unfreezed;
-				$body = $this->func->edit_form($page, $postdata);
+				$msg  = $this->root->_title_unfreezed;
+				$redirect = $this->root->script . '?cmd=edit&page=' . rawurlencode($page);
 			} else {
-				$this->root->vars['cmd'] = 'read';
-				$msg  = & $this->root->_title_unfreezed;
-				$body = '';
+				$msg  = $this->root->_title_unfreezed;
 			}
-	
+
 		} else {
 			// Show unfreeze form
 			$msg    = & $this->root->_title_unfreeze;
@@ -72,8 +65,8 @@ class xpwiki_plugin_unfreeze extends xpwiki_plugin {
 </form>
 EOD;
 		}
-	
-		return array('msg'=>$msg, 'body'=>$body);
+
+		return array('msg'=>$msg, 'body'=>$body, 'redirect'=>$redirect);
 	}
 }
 ?>
