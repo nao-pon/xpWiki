f0b00d3e8edf8d28a05fdbe4a13ddec087da7c2b
nao-pon <nao-pon>
2010-01-08 13:50:42 +0000

    Supported "$uname" for default value.

--- plugin/lookup.inc.php
+++ plugin/lookup.inc.php
@@ -4,36 +4,37 @@ class xpwiki_plugin_lookup extends xpwiki_plugin {
 
 
 	// PukiWiki - Yet another WikiWikiWeb clone.
-	// $Id: lookup.inc.php,v 1.3 2007/09/19 11:27:15 nao-pon Exp $
+	// $Id: lookup.inc.php,v 1.4 2010/01/08 13:50:42 nao-pon Exp $
 	// Copyright (C)
 	//   2002-2005 PukiWiki Developers Team
 	//   2001-2002 Originally written by yu-ji
 	// License: GPL v2 or (at your option) any later version
 	//
 	// InterWiki lookup plugin
-	
+
 		$this->cont['PLUGIN_LOOKUP_USAGE'] =  '#lookup(interwikiname[,button_name[,default]])';
 
 	}
-	
+
 	function plugin_lookup_convert()
 	{
 	//	global $vars;
 	//	static $id = 0;
 		static $id = array();
 		if (!isset($id[$this->xpwiki->pid])) {$id[$this->xpwiki->pid] = 0;}
-	
+
 		$num = func_num_args();
 		if ($num == 0 || $num > 3) return $this->cont['PLUGIN_LOOKUP_USAGE'];
-	
+
 		$args = func_get_args();
 		$interwiki = htmlspecialchars(trim($args[0]));
 		$button    = isset($args[1]) ? trim($args[1]) : '';
 		$button    = ($button != '') ? htmlspecialchars($button) : 'lookup';
 		$default   = ($num > 2) ? htmlspecialchars(trim($args[2])) : '';
+		$default = str_replace('$uname', $this->cont['USER_NAME_REPLACE'], $default);
 		$s_page    = htmlspecialchars($this->root->vars['page']);
 		++$id[$this->xpwiki->pid];
-	
+
 		$script = $this->func->get_script_uri();
 		$ret = <<<EOD
 <form action="$script" method="post">
@@ -49,23 +50,23 @@ class xpwiki_plugin_lookup extends xpwiki_plugin {
 EOD;
 		return $ret;
 	}
-	
+
 	function plugin_lookup_action()
 	{
 	//	global $post; // Deny GET method to avlid GET loop
-	
+
 		$page  = isset($this->root->post['page'])  ? $this->root->post['page']  : '';
 		$inter = isset($this->root->post['inter']) ? $this->root->post['inter'] : '';
 		if ($page === '') return FALSE; // Do nothing
 		if ($inter === '') return array('msg'=>'Invalid access', 'body'=>'');
-	
+
 		$url = $this->func->get_interwiki_url($inter, $page);
 		if ($url === FALSE) {
 			$msg = sprintf('InterWikiName "%s" not found', $inter);
 			$msg = htmlspecialchars($msg);
 			return array('msg'=>'Not found', 'body'=>$msg);
 		}
-	
+
 		$this->func->send_location('', '', $url);
 	}
 }
