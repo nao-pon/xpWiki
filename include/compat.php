304ea42a3cac5818e8461b6fc5774cd0849a6f0a
nao-pon <nao-pon>
2010-05-19 11:27:42 +0000

    "array_combine()" added.

--- include/compat.php
+++ include/compat.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Created on 2006/10/16 by nao-pon http://hypweb.net/
-// $Id: compat.php,v 1.8 2009/10/22 08:51:34 nao-pon Exp $
+// $Id: compat.php,v 1.9 2010/05/19 11:27:42 nao-pon Exp $
 //
 
 //// mbstring ////
@@ -21,7 +21,7 @@ if (! function_exists('is_a')) {
 
 	function is_a($class, $match)
 	{
-		if (empty($class)) return FALSE; 
+		if (empty($class)) return FALSE;
 
 		$class = is_object($class) ? get_class($class) : $class;
 		if (strtolower($class) == strtolower($match)) {
@@ -81,7 +81,7 @@ if (! function_exists('file_get_contents')) {
 			trigger_error('file_get_contents() failed to open stream: No such file or directory', E_USER_WARNING);
 			return false;
 		}
- 
+
 		clearstatcache();
 		if ($fsize = @filesize($filename)) {
 			$data = fread($fh, $fsize);
@@ -91,7 +91,7 @@ if (! function_exists('file_get_contents')) {
 				$data .= fread($fh, 8192);
 			}
 		}
- 
+
 		fclose($fh);
 		return $data;
 	}
@@ -118,3 +118,44 @@ if(!function_exists("array_change_key_case")){
         return $product;
     }/* endfunction array_change_key_case */
 }/* endfunction exists array_change_key_case*/
+
+// array_combine
+// (PHP 5)
+if (! function_exists('array_combine')) {
+function array_combine($keys, $values)
+{
+	if (!is_array($keys)) {
+		user_error('array_combine() expects parameter 1 to be array, ' .
+			gettype($keys) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	if (!is_array($values)) {
+		user_error('array_combine() expects parameter 2 to be array, ' .
+			gettype($values) . ' given', E_USER_WARNING);
+		return;
+	}
+
+	$key_count = count($keys);
+	$value_count = count($values);
+	if ($key_count !== $value_count) {
+		user_error('array_combine() Both parameters should have equal number of elements', E_USER_WARNING);
+		return false;
+	}
+
+	if ($key_count === 0 || $value_count === 0) {
+		user_error('array_combine() Both parameters should have number of elements at least 0', E_USER_WARNING);
+		return false;
+	}
+
+	$keys	 = array_values($keys);
+	$values	 = array_values($values);
+
+	$combined = array();
+	for ($i = 0; $i < $key_count; $i++) {
+		$combined[$keys[$i]] = $values[$i];
+	}
+
+	return $combined;
+}
+}
