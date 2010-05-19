fda34b0d62f97f9853644ab08e5b8fb1cbb49d27
nao-pon <nao-pon>
2010-05-19 11:36:21 +0000

    Bug fix of problem that "' " adheres to address display.

--- plugin/googlemaps2_insertmarker.inc.php
+++ plugin/googlemaps2_insertmarker.inc.php
@@ -16,47 +16,47 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 
 		// 言語ファイルの読み込み
 		$this->load_language();
-			
+
 		$this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_DIRECTION'] =  'down'; //追加していく方向(up, down)
 		$this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_TITLE_MAXLEN'] =  40; //タイトルの最長の長さ
 		$this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_CAPTION_MAXLEN'] =  400; //キャプションの最長の長さ
 		$this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_URL_MAXLEN'] =  1024; //URLの最長の長さ
 
 	}
-	
+
 	function plugin_googlemaps2_insertmarker_action() {
-	
+
 		if ($this->cont['PKWK_READONLY']) $this->func->die_message('PKWK_READONLY prohibits editing');
-		
+
 		if(is_numeric($this->root->vars['lat'])) $lat = $this->root->vars['lat']; else return;
 		if(is_numeric($this->root->vars['lng'])) $lng = $this->root->vars['lng']; else return;
 		if(is_numeric($this->root->vars['zoom'])) $zoom = $this->root->vars['zoom']; else return;
 		if(is_numeric($this->root->vars['mtype'])) $mtype = $this->root->vars['mtype']; else return;
-		
+
 		$maptypes = array(
 			1 => 'satellite',
 			2 => 'hybrid',
 			3 => 'physical',
 		);
 		$mtypename = isset($maptypes[$mtype])? $maptypes[$mtype] : 'normal';
-	
+
 		$map    = htmlspecialchars(trim($this->root->vars['map']));
 		$icon   = htmlspecialchars($this->root->vars['icon']);
 		$title   = substr($this->root->vars['title'], 0, $this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_TITLE_MAXLEN']);
 		$caption = substr(trim($this->root->vars['caption']), 0, $this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_CAPTION_MAXLEN']);
 		$image   = substr($this->root->vars['image'], 0, $this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_URL_MAXLEN']);
 		$maxurl  = substr($this->root->vars['maxurl'], 0, $this->cont['PLUGIN_GOOGLEMAPS2_INSERTMARKER_URL_MAXLEN']);
-		
+
 		$minzoom = $this->root->vars['minzoom'] == '' ? '' : (int)$this->root->vars['minzoom'];
 		$maxzoom = $this->root->vars['maxzoom'] == '' ? '' : (int)$this->root->vars['maxzoom'];
-		
+
 		$caption .= (isset($this->root->vars['save_addr']))? ($caption? '&br;' : '') . $this->msg['cap_addr'] . ': ' . $this->root->vars['addr'] : '';
-	
+
 		$title   = htmlspecialchars(str_replace("\n", '', $title));
 		$caption = str_replace("\n", '&br;', $caption);
 		$image   = htmlspecialchars($image);
 		$maxurl  = htmlspecialchars($maxurl);
-	
+
 		$marker = '-&googlemaps2_mark('.$lat.', '.$lng;
 		if ($title)         $marker .= ', title='.$title;
 		if ($map)           $marker .= ', map='.$map;
@@ -69,11 +69,11 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 		if (!empty($this->root->vars['save_zoom'])) $marker .= ', zoom='.$zoom;
 		if (!empty($this->root->vars['save_mtype'])) $marker .= ', type='.$mtypename;
 		$marker .= '){'.$caption.'};';
-		
+
 		$no       = 0;
 		$postdata = '';
 		$above    = ($this->root->vars['direction'] == 'up');
-		
+
 		$postdata_old = $this->func->get_source($this->root->vars['refer']);
 		$this->func->escape_multiline_pre($postdata_old, TRUE);
 		foreach ($postdata_old as $line) {
@@ -87,21 +87,21 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 			}
 			if ($above) $postdata .= $line;
 		}
-	
+
 		$title = $this->root->_title_updated;
 		$body = '';
 		if ($this->func->get_digests($this->func->get_source($this->root->vars['refer'], TRUE, TRUE)) != $this->root->vars['digest']) {
 			$title = $this->root->_title_comment_collided;
 			$body  = $this->root->_msg_comment_collided . $this->func->make_pagelink($this->root->vars['refer']);
 		}
-	
+
 		$this->func->escape_multiline_pre($postdata, FALSE);
 		$this->func->page_write($this->root->vars['refer'], $postdata);
-	
+
 		$retvars['msg']  = $title;
 		$retvars['body'] = $body;
 		$this->root->vars['page'] = $this->root->vars['refer'];
-	
+
 		//表示していたポジションを返すcookieを追加
 		$cookieval = 'lat|'.$lat.'|lng|'.$lng.'|zoom|'.$zoom.'|mtype|'.$mtype;
 		if ($minzoom) $cookieval .= '|minzoom|'.$minzoom;
@@ -109,7 +109,7 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 		setcookie('pukiwkigooglemaps2insertmarker'.$this->root->vars['no'], $cookieval);
 		return $retvars;
 	}
-	
+
 	function plugin_googlemaps2_insertmarker_get_default() {
 	//	global $vars;
 		return array(
@@ -123,27 +123,27 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 	//}
 	function plugin_googlemaps2_insertmarker_convert() {
 		static $numbers = array();
-		
+
 		if (!isset($numbers[$this->xpwiki->pid])) {$numbers[$this->xpwiki->pid] = array();}
 
 		$p_googlemaps2 =& $this->func->get_plugin_instance('googlemaps2');
-				
+
 		if ($p_googlemaps2->plugin_googlemaps2_is_supported_profile() && !$p_googlemaps2->lastmap_name) {
 			return "googlemaps2_insertmarker: {$p_googlemaps2->msg['err_need_googlemap2']}";
 		}
-		
+
 		if (!$p_googlemaps2->plugin_googlemaps2_is_supported_profile()) {
 			return '';
 		}
-	
+
 		if ($this->cont['PKWK_READONLY']) {
 			return "read only<br>";
 		}
-		
+
 		$this->msg['default_icon_caption'] = $p_googlemaps2->msg['default_icon_caption'];
-		
+
 		//オプション
-		
+
 		$defoptions = $this->plugin_googlemaps2_insertmarker_get_default();
 		$inoptions = array();
 		foreach (func_get_args() as $param) {
@@ -153,14 +153,14 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 			$value = htmlspecialchars(trim(substr($param, $pos+1)), ENT_QUOTES);
 			$inoptions[$index] = $value;
 		}
-	
+
 		if (array_key_exists('define', $inoptions)) {
 			$this->root->vars['googlemaps2_insertmarker'][$inoptions['define']] = $inoptions;
 			return '';
 		}
-		
+
 		$this->func->add_tag_head('googlemaps2.css');
-		
+
 		$coptions = array();
 		if (array_key_exists('class', $inoptions)) {
 			$class = $inoptions['class'];
@@ -180,11 +180,11 @@ class xpwiki_plugin_googlemaps2_insertmarker extends xpwiki_plugin {
 		$this->root->script    = $this->func->get_script_uri();
 		$s_page    = htmlspecialchars($this->root->vars['page']);
 		$page = $p_googlemaps2->get_pgid($this->root->vars['page']);
-		
+
 		if (! isset($numbers[$this->xpwiki->pid][$page]))
 			$numbers[$this->xpwiki->pid][$page] = 0;
 		$no = $numbers[$this->xpwiki->pid][$page]++;
-	
+
 		$imprefix = "_p_googlemaps2_insertmarker_".$page."_".$no;
 		$script = $this->func->get_script_uri();
 		$output = <<<EOD
@@ -269,7 +269,7 @@ onloadfunc.push(function() {
 		var form  = document.getElementById("${imprefix}_form");
 		var icon  = document.getElementById("${imprefix}_icon");
 		var addr  = document.getElementById("${imprefix}_addr");
-		
+
 		var update_func = function() {
 			lat.value = PGTool.fmtNum(map.getCenter().lat());
 			lng.value = PGTool.fmtNum(map.getCenter().lng());
@@ -287,11 +287,11 @@ onloadfunc.push(function() {
 			}
 			//geocoder.getLocations(new GLatLng(lat.value, lng.value), set_addr);
 		};
-		
+
 		var update_addr = function() {
 			geocoder.getLocations(new GLatLng(map.getCenter().lat(), map.getCenter().lng()), set_addr);
 		}
-		
+
 		var set_addr = function(response) {
 			if (!response || response.Status.code != 200) {
 				addr.value = '';
@@ -302,24 +302,24 @@ onloadfunc.push(function() {
 					place = response.Placemark[i++];
 				}
 				if (place.AddressDetails.Country.CountryNameCode == 'JP') {
-					addr.value = place.address.replace(new RegExp('^' + place.AddressDetails.Country.CountryName), '');
+					addr.value = place.address.replace(new RegExp('^' + place.AddressDetails.Country.CountryName + '(, *)?'), '');
 				} else {
 					addr.value = place.address;
 				}
 			}
 		}
-		
+
 		//Whenever the map is dragged, the parameter is dynamically substituted.
 		GEvent.addListener(map, 'moveend', update_func);
 		GEvent.addListener(map, 'maptypechanged', update_func);
 		GEvent.addListener(map, 'zoom', update_func);
-		
+
 		GEvent.addListener(map, 'moveend', update_addr);
-		
+
 		update_func();
 		update_addr();
-		
-		//The position of the map is initialized if there is a cookie. Contents of the cookie are cleared when finishing using it. 
+
+		//The position of the map is initialized if there is a cookie. Contents of the cookie are cleared when finishing using it.
 		(function () {
 			var cookies = document.cookie.split(";");
 			for (i in cookies) {
@@ -346,7 +346,7 @@ onloadfunc.push(function() {
 								default: j++; break;
 							}
 						}
-						map.setCenter(new GLatLng(mparam.lat, mparam.lng), 
+						map.setCenter(new GLatLng(mparam.lat, mparam.lng),
 								mparam.zoom, map.getMapTypes()[mparam.mtype]);
 
 						var smz;
@@ -379,7 +379,7 @@ onloadfunc.push(function() {
 
 		//Input check
 		form.onsubmit = function () {
-			if (isNaN(parseFloat(lat.value)) || isNaN(lat.value) || 
+			if (isNaN(parseFloat(lat.value)) || isNaN(lat.value) ||
 				isNaN(parseFloat(lng.value)) || isNaN(lng.value)) {
 				alert("{$this->msg['err_irreg_dat']} LAT : " + lat.value + "  LNG : " + lng.value);
 				return false;
@@ -387,7 +387,7 @@ onloadfunc.push(function() {
 			return true;
 		};
 	}
-	//The selection is updated reading all the icon definitions that exist on this page. 
+	//The selection is updated reading all the icon definitions that exist on this page.
 	onloadfunc.push(function() {
 		for(iconname in googlemaps_icons['$page']) {
 			if (!googlemaps_icons['$page'].hasOwnProperty(iconname) || iconname == 'Default') continue;
@@ -401,7 +401,7 @@ onloadfunc.push(function() {
 //]]>
 </script>
 EOD;
-	
+
 		return $output;
 	}
 }
