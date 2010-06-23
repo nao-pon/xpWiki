4df3a7c9ecf8d7d7218b48d3af5ace706f9a32c1
nao-pon <hypweb@gmail.com>
2010-06-23 07:25:44 +0000

    Always uses the "MobilePictogramConverter".

--- plugin/edit.inc.php
+++ plugin/edit.inc.php
@@ -1,21 +1,21 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: edit.inc.php,v 1.70 2009/10/22 08:43:54 nao-pon Exp $
+// $Id: edit.inc.php,v 1.71 2010/06/23 07:25:44 nao-pon Exp $
 // Copyright (C) 2001-2006 PukiWiki Developers Team
 // License: GPL v2 or (at your option) any later version
 //
 // Edit plugin (cmd=edit)
-	
+
 class xpwiki_plugin_edit extends xpwiki_plugin {
 	function plugin_edit_init () {
 		// Remove #freeze written by hand
 		$this->cont['PLUGIN_EDIT_FREEZE_REGEX'] = '/^(?:#freeze(?!\w)\s*)+/im';
 	}
-	
+
 	function plugin_edit_action()
 	{
 		if ($this->cont['PKWK_READONLY']) $this->func->die_message('PKWK_READONLY prohibits editing');
-	
+
 		$page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
 
 		// check alias page
@@ -27,7 +27,7 @@ class xpwiki_plugin_edit extends xpwiki_plugin {
 			$this->func->get_pagename_realcase($page);
 			$this->root->get['page'] = $this->root->post['page'] = $this->root->vars['page'] = $page;
 		}
-		
+
 		$this->func->check_editable($page, true, true);
 
 		if (isset($this->root->vars['write'])) {
@@ -41,16 +41,16 @@ class xpwiki_plugin_edit extends xpwiki_plugin {
 		} else if (isset($this->root->vars['cancel'])) {
 			return $this->plugin_edit_cancel();
 		}
-	
+
 		$title = $source = '';
-		
+
 		if (!empty($this->root->get['backup'])) {
 			$backup_age = intval($this->root->get['backup']);
 			$backup = $this->func->get_backup($page, $backup_age);
 			$source = $backup['data'];
 			$title = '$1 - ' . str_replace('$1', $backup_age, $this->root->_msg_backupedit);
 		}
-		
+
 		if (!$source) {
 			$source = $this->func->get_source($page);
 		}
@@ -66,12 +66,12 @@ class xpwiki_plugin_edit extends xpwiki_plugin {
 		}
 
 		if ($postdata == '') $postdata = $this->func->auto_template($page);
-		
+
 		// Q & A 認証
 		$options = $this->get_riddle();
-		
+
 		$body = $this->func->edit_form($page, $postdata, FALSE, TRUE, $options);
-		
+
 		if (isset($this->root->vars['ajax'])) {
 			$this->func->convert_finisher($body);
 			$body = <<<EOD
@@ -79,27 +79,27 @@ class xpwiki_plugin_edit extends xpwiki_plugin {
 EOD;
 			$this->func->send_xml($body);
 		}
-		
+
 		return array('msg' => ($title ? $title : $this->root->_title_edit), 'body' => $body);
 	}
-	
+
 	// Preview
 	function plugin_edit_preview($ng_riddle = FALSE)
 	{
 		$page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
-	
+
 		// Loading template
 		if (! $ng_riddle && isset($this->root->vars['template_page']) && $this->func->is_page($this->root->vars['template_page'])) {
-	
+
 			$this->root->vars['msg'] = $this->func->get_source($this->root->vars['template_page'], TRUE, TRUE);
-	
+
 			// Cut fixed anchors
 			$this->root->vars['msg'] = preg_replace('/^(\*{1,5}.*)\[#[A-Za-z][\w-]+\](.*)$/m', '$1$2', $this->root->vars['msg']);
 		}
-	
+
 		$this->root->vars['msg'] = preg_replace($this->cont['PLUGIN_EDIT_FREEZE_REGEX'], '', $this->root->vars['msg']);
 		$postdata = $this->root->vars['msg'];
-		
+
 		if (isset($this->root->vars['add']) && $this->root->vars['add']) {
 			if (isset($this->root->vars['add_top']) && $this->root->vars['add_top']) {
 				$postdata  = $postdata . "\n\n" . $this->func->get_source($page, TRUE, TRUE);
@@ -107,12 +107,12 @@ EOD;
 				$postdata  = $this->func->get_source($page, TRUE, TRUE) . "\n\n" . $postdata;
 			}
 		}
-	
+
 		$body = $this->root->_msg_preview . '<br />' . "\n";
 		if ($postdata == '') {
 			$body .= '<strong>' . $this->root->_msg_preview_delete . '</strong><br />' . "\n";
 		}
-		
+
 		$this->root->rtf['preview'] = TRUE;
 		if ($postdata) {
 			$postdata = $this->func->make_str_rules($postdata);
@@ -143,7 +143,7 @@ new Resizable('xpwiki_preview_area', {mode:'y'});
 </script>
 EOD;
 		}
-		
+
 		// Q & A 認証
 		$options = $this->get_riddle();
 
@@ -160,14 +160,14 @@ EOD;
 					$body);
 			$body = preg_replace('/<div id="(xpwiki_body|'.preg_quote($this->root->vars['paraid'], '/').')"/', '<div ', $body);
 			$body .= $this->func->edit_form($page, $this->root->vars['msg'], $this->root->vars['digest'], TRUE, $options);
-			
+
 			$this->func->convert_finisher($body);
-			
+
 			$title = (!$ng_riddle)? $this->root->_title_preview : $this->root->_title_ng_riddle;
 			$title = '<h3>'.str_replace('$1', htmlspecialchars($page), $title).'</h3>';
 			$body = $title.$body;
 
-			if (defined('HYP_K_TAI_RENDER') && preg_match('/\(\([eisv]:[0-9a-f]{4}\)\)|\[emj:\d{1,4}(?::(?:im|ez|sb))?\]/S', $body)) {
+			if (preg_match('/\(\([eisv]:[0-9a-f]{4}\)\)|\[emj:\d{1,4}(?::(?:im|ez|sb))?\]/S', $body)) {
 				if (! XC_CLASS_EXISTS('MobilePictogramConverter')) {
 					HypCommonFunc::loadClass('MobilePictogramConverter');
 				}
@@ -189,10 +189,10 @@ EOD;
 		} else {
 			$body .= $this->func->edit_form($page, $this->root->vars['msg'], (isset($this->root->vars['digest'])? $this->root->vars['digest'] : FALSE), TRUE, $options);
 		}
-		
+
 		return array('msg'=>(!$ng_riddle)? $this->root->_title_preview : $this->root->_title_ng_riddle, 'body'=>$body);
 	}
-	
+
 	// Inline: Show edit (or unfreeze text) link
 	function plugin_edit_inline()
 	{
@@ -200,15 +200,15 @@ EOD;
 		if (!isset($usage[$this->xpwiki->pid])) {
 			$usage[$this->xpwiki->pid] = '&edit(pagename#anchor[[,noicon],nolabel])[{label}];';
 		}
-	
-		if ($this->cont['PKWK_READONLY']) return ''; // Show nothing 
-	
+
+		if ($this->cont['PKWK_READONLY']) return ''; // Show nothing
+
 		// Arguments
 		$args = func_get_args();
-	
+
 		// {label}. Strip anchor tags only
 		$s_label = $this->func->strip_htmltag(array_pop($args), FALSE);
-	
+
 		$page    = array_shift($args);
 		if ($page === NULL) $page = '';
 		$_noicon = $_nolabel = $_paraedit = FALSE;
@@ -222,22 +222,22 @@ EOD;
 			}
 		}
 		if ($_paraedit) $_nolabel = TRUE;
-		
+
 		// Separate a page-name and a fixed anchor
 		list($s_page, $id, $editable) = $this->func->anchor_explode($page, TRUE);
-		
+
 		// Default: This one
 		if ($s_page === '') $s_page = isset($this->root->vars['page']) ? $this->root->vars['page'] : '';
 
 		// 編集権限チェック
 		$is_editable = $this->func->check_editable($s_page,FALSE,FALSE);
-	
+
 		// $s_page fixed
 		$isfreeze = $this->func->is_freeze($s_page);
 		$ispage   = $this->func->is_page($s_page);
 		//if ($_paraedit && ($isfreeze || !$is_editable)) return ''; // Show nothing
 		if ($isfreeze || !$is_editable) return ''; // Show nothing
-	
+
 		// Paragraph edit enabled or not
 		$short = htmlspecialchars('Edit');
 		$js = $ajax = '';
@@ -280,7 +280,7 @@ EOD;
 		} else {
 			if ($s_label == '') $s_label = $title; // Rich label with an icon
 		}
-	
+
 		// URL
 		if ($isfreeze) {
 			$url   = $this->root->script . '?cmd=unfreeze&amp;page=' . rawurlencode($s_page);
@@ -291,7 +291,7 @@ EOD;
 		$atag  = '<a' . $class . ' href="' . $url . '" title="' . $title . '"' . $js . $ajax . '>';
 		static $atags = array();
 		if (!isset($atags[$this->xpwiki->pid])) {$atags[$this->xpwiki->pid] = '</a>';}
-		
+
 		if (!empty($this->root->rtf['preview'])) {
 			// Preview mode
 			return $icon . $s_label;
@@ -304,12 +304,12 @@ EOD;
 			$s_label . $atag . '?' . $atags[$this->xpwiki->pid] . '</span>';
 		}
 	}
-	
+
 	// Write, add, or insert new comment
 	function plugin_edit_write()
 	{
 		$_uname = (empty($this->root->vars['uname']) || !empty($this->root->vars['anonymous']))? $this->root->siteinfo['anonymous'] : $this->root->vars['uname'];
-		if ($_uname) { 
+		if ($_uname) {
 			if (! empty($this->root->vars['anonymous'])) {
 				$this->root->cookie['name'] = $_uname;
 			} else {
@@ -317,13 +317,13 @@ EOD;
 				$this->func->save_name2cookie($_uname);
 			}
 		}
-		
+
 		$page   = isset($this->root->vars['page'])   ? $this->root->vars['page']   : '';
 		$add    = isset($this->root->vars['add'])    ? $this->root->vars['add']    : '';
 		$digest = isset($this->root->vars['digest']) ? $this->root->vars['digest'] : '';
 		$paraid = isset($this->root->vars['paraid']) ? $this->root->vars['paraid'] : '';
 		$original = '';
-		
+
 		$this->root->vars['msg'] = preg_replace($this->cont['PLUGIN_EDIT_FREEZE_REGEX'], '', $this->root->vars['msg']);
 		$this->root->vars['msg'] = $this->func->remove_pginfo($this->root->vars['msg']);
 		$msg = & $this->root->vars['msg']; // Reference
@@ -333,7 +333,7 @@ EOD;
 			$original = (string)$this->func->cache_get_db($this->root->vars['orgkey'], 'edit');
 			$original = $this->func->remove_pginfo($original);
 		}
-		
+
 		// ParaEdit
 		$hash = '';
 		if ($paraid) {
@@ -351,35 +351,35 @@ EOD;
 			$msg = $fullmsg;
 			$hash = '#' . $paraid;
 		}
-		
+
 		// 文末処理
 		$msg = rtrim($msg)."\n";
-		
+
 		// 改行・TAB・スペースのみだったら削除とみなす
 		$msg = preg_replace('/^\s+$/', '', $msg);
-		
+
 		// Page title
 		if ($msg && ! empty($this->root->post['pgtitle'])) {
 			$msg = $this->root->title_setting_string . trim($this->root->post['pgtitle']) . "\n" . $msg;
 		}
-	
+
 		$retvars = array();
-	
+
 		// Collision Detection
 		$oldpagesrc = $this->func->get_source($page, TRUE, TRUE);
 		$oldpagemd5 = $this->func->get_digests($oldpagesrc);
 		if ($digest != $oldpagemd5) {
 			$this->root->vars['digest'] = $oldpagemd5; // Reset
 			unset($this->root->vars['paraid']); // 更新が衝突したら全文編集に切り替え
-	
+
 			$oldpagesrc = $this->func->remove_pginfo($oldpagesrc);
 			list($postdata_input, $auto) = $this->func->do_update_diff($oldpagesrc, $msg, $original);
-	
+
 			$retvars['msg' ] = $this->root->_title_collided;
 			$retvars['body'] = ($auto ? $this->root->_msg_collided_auto : $this->root->_msg_collided) . "\n";
 			$retvars['body'] .= $this->root->do_update_diff_table;
 			$retvars['body'] .= $this->func->edit_form($page, $postdata_input, $oldpagemd5, FALSE);
-			
+
 			if (isset($this->root->vars['ajax'])) {
 				$this->func->convert_finisher($retvars['body']);
 				$body = <<<EOD
@@ -393,7 +393,7 @@ EOD;
 
 			return $retvars;
 		}
-	
+
 		// Action?
 		if ($add) {
 			// Add
@@ -410,9 +410,9 @@ EOD;
 		// NULL POSTING, OR removing existing page
 		if (! $postdata) {
 			$this->func->page_write($page, '');
-	
+
 			if ($this->root->trackback) $this->func->tb_delete($page);
-			
+
 			if ($this->root->maxshow_deleted && $this->func->is_page($this->root->whatsdeleted)) {
 				$url = $this->func->get_page_uri($this->root->whatsdeleted , true);
 			} else {
@@ -431,16 +431,16 @@ EOD;
 EOD;
 				$this->func->send_xml($body);
 			}
-			
+
 			$this->func->redirect_header($url, 1, $title);
 		}
-	
+
 		// $notimeupdate: Checkbox 'Do not change timestamp'
 		$notimestamp = isset($this->root->vars['notimestamp']) && $this->root->vars['notimestamp'] != '';
 		if ($this->root->notimeupdate > 1 && ! $this->root->userinfo['admin']) {
 			$notimestamp = false;
 		}
-		
+
 		$this->func->page_write($page, $postdata, $this->root->notimeupdate != 0 && $notimestamp);
 
 		if (isset($this->root->vars['ajax'])) {
@@ -461,8 +461,8 @@ EOD;
 					}
 					$body = str_replace(array('<![CDATA[', ']]>'), '', $body);
 				}
-				
-				if (defined('HYP_K_TAI_RENDER') && preg_match('/\(\([eisv]:[0-9a-f]{4}\)\)|\[emj:\d{1,4}(?::(?:im|ez|sb))?\]/S', $body)) {
+
+				if (preg_match('/\(\([eisv]:[0-9a-f]{4}\)\)|\[emj:\d{1,4}(?::(?:im|ez|sb))?\]/S', $body)) {
 					if (! XC_CLASS_EXISTS('MobilePictogramConverter')) {
 						HypCommonFunc::loadClass('MobilePictogramConverter');
 					}
@@ -486,7 +486,7 @@ EOD;
 
 		$this->func->send_location($page, $hash);
 	}
-	
+
 	// Cancel (Back to the page / Escape edit page)
 	function plugin_edit_cancel()
 	{
@@ -507,13 +507,13 @@ EOD;
 		}
 		$this->func->send_location($ret, $hash);
 	}
-	
+
 	// ソースの一部を抽出/置換する
 	function plugin_edit_parts($id, & $source, $postdata = '')
 	{
 		$id = preg_quote($id, '/');
 		$postdata = rtrim($postdata)."\n\n";
-		
+
 		// 改行・TAB・スペースのみだったら削除とみなす
 		$postdata = preg_replace('/^[ \s]+$/', '', $postdata);
 
@@ -553,21 +553,21 @@ EOD;
 				return join('', array_splice($source, $start, $final - $start, $postdata));
 			}
 		} else {
-		
+
 			$heads = preg_grep('/^\*{1,5}.+\[#[A-Za-z][\w-]+\].*$/', $source);
 			$heads[count($source)] = ''; // Sentinel
-		
+
 			while (list($start, $line) = each($heads)) {
 				if (preg_match('/\[#' . $id . '\]/', $line)) {
 					list($end, $line) = each($heads);
 					return join('', array_splice($source, $start, $end - $start, $postdata));
 				}
 			}
-			
+
 		}
 		return FALSE;
 	}
-	
+
 	// Q & A 認証用 $option 取得
 	function get_riddle () {
 		if ($this->root->userinfo['admin'] ||
@@ -578,7 +578,7 @@ EOD;
 		} else {
 			$options['riddle'] = array_rand($this->root->riddles);
 		}
-		return $options;	
+		return $options;
 	}
 }
 ?>
