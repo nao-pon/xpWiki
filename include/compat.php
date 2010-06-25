338b4e8dee478d907f43df621c7ca3f34cf1caf4
nao-pon <nao-pon>
2010-06-25 07:54:07 +0000

    Added a function "file_put_contents()" & edited a function "file_put_contents".

--- include/compat.php
+++ include/compat.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Created on 2006/10/16 by nao-pon http://hypweb.net/
-// $Id: compat.php,v 1.9 2010/05/19 11:27:42 nao-pon Exp $
+// $Id: compat.php,v 1.10 2010/06/25 07:54:07 nao-pon Exp $
 //
 
 //// mbstring ////
@@ -74,16 +74,29 @@ if (! function_exists('sha1')) {
 
 // file_get_contents -- Reads entire file into a string
 // (PHP 4 >= 4.3.0, PHP 5)
-if (! function_exists('file_get_contents')) {
-	function file_get_contents($filename, $incpath = false, $resource_context = null)
-	{
-		if (false === $fh = fopen($filename, 'rb', $incpath)) {
-			trigger_error('file_get_contents() failed to open stream: No such file or directory', E_USER_WARNING);
-			return false;
-		}
+if (! function_exists('xpwiki_file_get_contents')) {
+function xpwiki_file_get_contents($filename, $incpath = false, $resource_context = null, $offset = -1, $maxlen = -1)
+{
+	if (false === $fh = fopen($filename, 'rb', $incpath)) {
+		trigger_error('file_get_contents() failed to open stream: No such file or directory', E_USER_WARNING);
+		return false;
+	}
 
-		clearstatcache();
-		if ($fsize = @filesize($filename)) {
+	if ($offset > -1 && $maxlen > -1) {
+		$readsize = $offset + $maxlen;
+	} else {
+		$readsize = -1;
+	}
+
+	clearstatcache();
+	$fsize = @filesize($filename);
+	if ($readsize > -1 && $fsize > $readsize) {
+		$data = fread($fh, $readsize);
+		if ($offset > 0) {
+			$data = substr($data, $offset);
+		}
+	} else {
+		if ($fsize) {
 			$data = fread($fh, $fsize);
 		} else {
 			$data = '';
@@ -91,10 +104,103 @@ if (! function_exists('file_get_contents')) {
 				$data .= fread($fh, 8192);
 			}
 		}
-
-		fclose($fh);
-		return $data;
 	}
+
+	fclose($fh);
+	return $data;
+}
+}
+if (! function_exists('file_get_contents')) {
+function file_get_contents($filename, $incpath = false, $resource_context = null, $offset = -1, $maxlen = -1)
+{
+	return xpwiki_file_get_contents($filename, $incpath, $resource_context, $offset, $maxlen);
+}
+}
+
+/**
+ * Replace file_put_contents()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @link        http://php.net/function.file_put_contents
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     $Revision: 1.10 $
+ * @internal    resource_context is not supported
+ * @since       PHP 5
+ * @require     PHP 4.0.0 (user_error)
+ */
+// file_put_contents
+// (PHP 5)
+if (! function_exists('file_put_contents')) {
+function file_put_contents($filename, $content, $flags = null, $resource_context = null)
+{
+    // If $content is an array, convert it to a string
+    if (is_array($content)) {
+        $content = implode('', $content);
+    }
+
+    // If we don't have a string, throw an error
+    if (!is_scalar($content)) {
+        user_error('file_put_contents() The 2nd parameter should be either a string or an array',
+            E_USER_WARNING);
+        return false;
+    }
+
+    // Get the length of data to write
+    $length = strlen($content);
+
+    // Check what mode we are using
+    $mode = ($flags & FILE_APPEND) ?
+                'a' :
+                'wb';
+
+    // Check if we're using the include path
+    $use_inc_path = ($flags & FILE_USE_INCLUDE_PATH) ?
+                true :
+                false;
+
+    // Open the file for writing
+    if (($fh = @fopen($filename, $mode, $use_inc_path)) === false) {
+        user_error('file_put_contents() failed to open stream: Permission denied',
+            E_USER_WARNING);
+        return false;
+    }
+
+    // Attempt to get an exclusive lock
+    $use_lock = ($flags & LOCK_EX) ? true : false ;
+    if ($use_lock === true) {
+        if (!flock($fh, LOCK_EX)) {
+            return false;
+        }
+    }
+
+    // Write to the file
+    $bytes = 0;
+    if (($bytes = @fwrite($fh, $content)) === false) {
+        $errormsg = sprintf('file_put_contents() Failed to write %d bytes to %s',
+                        $length,
+                        $filename);
+        user_error($errormsg, E_USER_WARNING);
+        return false;
+    }
+
+    // Close the handle
+    @fclose($fh);
+
+    // Check all the data was written
+    if ($bytes != $length) {
+        $errormsg = sprintf('file_put_contents() Only %d of %d bytes written, possibly out of free disk space.',
+                        $bytes,
+                        $length);
+        user_error($errormsg, E_USER_WARNING);
+        return false;
+    }
+
+    // Return length
+    return $bytes;
+}
 }
 
 // array_change_key_case
