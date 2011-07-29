a04c203fca92d71bca60fbf4f406780b823c4b3e
nao-pon <hypweb@gmail.com>
2011-07-29 01:37:52 +0000

    Setting for "pagename_normalize()".

--- language/xpwiki/Conf_ISO-8859-1.php
+++ language/xpwiki/Conf_ISO-8859-1.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/10/11 by nao-pon http://hypweb.net/
- * $Id: Conf_ISO-8859-1.php,v 1.1 2007/11/28 05:56:38 nao-pon Exp $
+ * $Id: Conf_ISO-8859-1.php,v 1.2 2011/07/29 01:37:52 nao-pon Exp $
  */
 
 // Encoding hint
@@ -9,4 +9,7 @@ $_LANG['encode_hint'] = 'ÿ';
 
 // Accept language
 $const['ACCEPT_UILANG'] = 'da,de,en,es,fr,la,no,pt,sv,it';
-?>
+
+// Array for normalization of page name. Characters that cannot be used.  [ ] < > # & " :
+$root->pagename_illegality = array('[', ']', '<', '>', '#',  '&',     '"',  ':');
+$root->pagename_normalizer = array('(', ')', '«', '»', '§', ' and ', '¨', '¦');
