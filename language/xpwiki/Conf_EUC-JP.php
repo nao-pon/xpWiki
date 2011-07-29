37aae647d129ae1db53204ae0daa8fa8bf8bd041
nao-pon <nao-pon>
2011-07-29 01:37:52 +0000

    Setting for "pagename_normalize()".

--- language/xpwiki/Conf_EUC-JP.php
+++ language/xpwiki/Conf_EUC-JP.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/10/11 by nao-pon http://hypweb.net/
- * $Id: Conf_EUC-JP.php,v 1.1 2007/11/28 05:56:38 nao-pon Exp $
+ * $Id: Conf_EUC-JP.php,v 1.2 2011/07/29 01:37:52 nao-pon Exp $
  */
 
 // Encoding hint
@@ -9,4 +9,7 @@ $_LANG['encode_hint'] = 'ぷ';
 
 // Accept language
 $const['ACCEPT_UILANG'] = 'ja,en';
-?>
+
+// ページ名の正規化用配列 使用できない文字 [ ] < > # & " :
+$root->pagename_illegality = array('[',  ']',  '<',  '>',  '#',  '&',  '"',  ':');
+$root->pagename_normalizer = array('［', '］', '＜', '＞', '＃', '＆', '”', '：');
