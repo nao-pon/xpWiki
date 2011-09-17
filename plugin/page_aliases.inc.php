04b30fce97c45f5686ba8b76bf4b5992c282e9eb
nao-pon <nao-pon>
2011-09-17 07:32:44 +0000

    Added inline mode, that is any page's aliases list.

--- plugin/page_aliases.inc.php
+++ plugin/page_aliases.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/08/30 by nao-pon http://hypweb.net/
- * $Id: page_aliases.inc.php,v 1.3 2011/09/09 07:35:57 nao-pon Exp $
+ * $Id: page_aliases.inc.php,v 1.4 2011/09/17 07:32:44 nao-pon Exp $
  */
 
 class xpwiki_plugin_page_aliases extends xpwiki_plugin {
@@ -10,12 +10,37 @@ class xpwiki_plugin_page_aliases extends xpwiki_plugin {
 		return array('msg'=>'Page aliases list', 'body'=>$this->get());
 	}
 
+	function can_call_otherdir_convert() {
+		return 1;
+	}
+
+	function can_call_otherdir_inline() {
+		return 1;
+	}
+
 	function plugin_page_aliases_convert() {
 		return $this->get();
 	}
 
-	function can_call_otherdir_convert() {
-		return 1;
+	function plugin_page_aliases_inline() {
+
+		$arg = func_get_args();
+
+		// get body "{...}"
+		$body = array_pop($arg);
+
+		$page = '';
+		if (isset($arg[0])) {
+			$page = $arg[0];
+		}
+		if (!$page || $this->func->is_page($page)) {
+			$page = $this->root->vars['page'];
+		}
+
+		$pagealiases = $this->func->get_page_alias($page, true, false, 'relative');
+
+		return $pagealiases? join(', ', $pagealiases) : $this->root->_LANG['skin']['none'];
+
 	}
 
 	function get() {
