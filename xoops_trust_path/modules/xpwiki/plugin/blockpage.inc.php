e3946dfec57568a5285ec9af310607559e30b0c1
nao-pon <hypweb@gmail.com>
2013-06-03 15:46:34 +0900

    [plugin:blockpage] use fetch_options()

--- xoops_trust_path/modules/xpwiki/plugin/blockpage.inc.php
+++ xoops_trust_path/modules/xpwiki/plugin/blockpage.inc.php
@@ -5,13 +5,15 @@ class xpwiki_plugin_blockpage extends xpwiki_plugin {
 	}
 
 	function plugin_blockpage_convert() {
+		$options = array();
 		$args = func_get_args();
-		if (! empty($args[0]) && strlen($args[0]) < 256 && isset($args[1])) {
-			$page = $this->func->get_fullname($args[1], $this->root->vars['page']);
+		$this->fetch_options($options, $args, array('id', 'page'));
+		if ( $options['id'] !== '' && strlen($options['id']) < 256 && $options['page'] !== '') {
+			$page = $this->func->get_fullname($options['page'], $this->root->vars['page']);
 			if (! $this->func->is_page($page)) {
 				return '#blockpage(ID,PAGENAME): No such page: ' . $this->func->make_pagelink($page);
 			} else {
-				$GLOBALS['Xpwiki_'.$this->root->mydirname]['cache']['blockpage'][$args[0]] = $page;
+				$GLOBALS['Xpwiki_'.$this->root->mydirname]['cache']['blockpage'][$options['id']] = $page;
 			}
 		} else {
 			return '#blockpage(ID,PAGENAME): Invalid ID or PageName.';
