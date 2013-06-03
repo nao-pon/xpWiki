1f3dceb31eae3afb2ffe27d5379254e7cfdbbc8f
nao-pon <hypweb@gmail.com>
2013-06-03 13:57:30 +0900

    [plugin:blockpage] fix syntax error

--- xoops_trust_path/modules/xpwiki/plugin/blockpage.inc.php
+++ xoops_trust_path/modules/xpwiki/plugin/blockpage.inc.php
@@ -3,10 +3,10 @@ class xpwiki_plugin_blockpage extends xpwiki_plugin {
 	function plugin_blockpage_init () {
 
 	}
-	
+
 	function plugin_blockpage_convert() {
 		$args = func_get_args();
-		if (! empty($args[0]) && strlen($args[0]) > 255 && isset($args[1])) {
+		if (! empty($args[0]) && strlen($args[0]) < 256 && isset($args[1])) {
 			$page = $this->func->get_fullname($args[1], $this->root->vars['page']);
 			if (! $this->func->is_page($page)) {
 				return '#blockpage(ID,PAGENAME): No such page: ' . $this->func->make_pagelink($page);
@@ -14,8 +14,9 @@ class xpwiki_plugin_blockpage extends xpwiki_plugin {
 				$GLOBALS['Xpwiki_'.$this->root->mydirname]['cache']['blockpage'][$args[0]] = $page;
 			}
 		} else {
-			return '#blockpage(ID,PAGENAME): Invalid ID or PageName.'
+			return '#blockpage(ID,PAGENAME): Invalid ID or PageName.';
 		}
 		return '';
 	}
 }
+
